5973f8bd6208d6bfea5d9d5195191a8a48cd5c92
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-09-24 22:13:20 +0300

    Create lowerOrUpper.js

---
+++ exerciseDataTypesAndVariables/lowerOrUpper.js
@@ -0,0 +1,9 @@
+function lowerOrUpper(input) {
+    let upperCase = input.toUpperCase()
+    if (input == upperCase) {
+        console.log('upper-case');
+    } else {
+        console.log('lower-case');
+    }
+}
+lowerOrUpper('l')
