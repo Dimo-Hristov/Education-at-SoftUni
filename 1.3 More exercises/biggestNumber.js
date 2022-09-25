b82f61002885059d18606b0a4ce126eb928db15f
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-09-25 21:47:44 +0300

    Create biggestNumber.js

---
+++ 1.3 More exercises/biggestNumber.js
@@ -0,0 +1,29 @@
+function biggestNum(num1, num2, num3) {
+    let biggestNum = 0;
+    if (num1 >= num2) {
+        if (num1 >= num3) {
+            biggestNum = num1
+        } else {
+            biggestNum = num3
+        }
+    } else if (num2 >= num1) {
+        if (num2 >= num3) {
+            biggestNum = num2
+        } else {
+            biggestNum = num3
+        }
+    } else if (num3 >= num1) {
+        if (num3 >= num2) {
+            biggestNum = num3
+        } else {
+            biggestNum = num2
+        }
+    }
+
+    console.log(biggestNum);
+}
+biggestNum(43,
+
+    43.2,
+
+    43.1)
