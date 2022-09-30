e567cc569c9a3c76721504ab42c2ce30085b7fef
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-09-30 23:01:08 +0300

    new version
    added hard way to solve and easy way to solve

--- 2.2 Exercise Arrays/commonElements.js
+++ 2.2 Exercise Arrays/commonElements.js
@@ -1,13 +1,24 @@
 function elements(arr1, arr2) {
-    for (let i = 0; i < arr1.length; i++) {
-        let arr1TempElement = arr1[i];
-        for (let i = 0; i < arr2.length; i++) {
-            let arr2TempElement = arr2[i]
-            if (arr1TempElement === arr2TempElement) {
-                console.log(arr1TempElement);
-            }
+    for (array1Elements of arr1) {
+        if (arr2.includes(array1Elements)) {
+            console.log(array1Elements);
         }
     }
+
+
+
+
+    //   Hard way
+
+    //     for (let i = 0; i < arr1.length; i++) {
+    //         let arr1TempElement = arr1[i];
+    //         for (let i = 0; i < arr2.length; i++) {
+    //             let arr2TempElement = arr2[i]
+    //             if (arr1TempElement === arr2TempElement) {
+    //                 console.log(arr1TempElement);
+    //             }
+    //         }
+    //     }
 
 }
 elements
