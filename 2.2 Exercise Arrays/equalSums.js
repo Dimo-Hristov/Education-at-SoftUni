150cdc74c74a06bf630b078c1debcfe2827dda0f
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-10-01 21:53:49 +0300

    edited

---
+++ 2.2 Exercise Arrays/equalSums.js
@@ -0,0 +1,33 @@
+function equalSums(array) {
+    let output = 'no';
+
+    for (let i = 0; i < array.length; i++) {
+        let currentNumber = array[i];
+        let rightSum = 0;
+        let leftSum = 0;
+        for (let j = 0; j < i; j++) {
+            rightSum += array[j];
+
+        }
+        for (let k = i + 1; k < array.length; k++) {
+            leftSum += array[k]
+        }
+        if (leftSum == rightSum) {
+            output = i
+        } else if (rightSum == 0 && leftSum == 0) {
+            output = 0
+        }
+    }
+    console.log(output);
+}
+equalSums([10,
+    5,
+    5,
+    99,
+    3,
+    4,
+    2,
+    5,
+    1,
+    1,
+    4])
--- 2.2 Exercise Arrays/maxNumber.js
+++ 2.2 Exercise Arrays/maxNumber.js
@@ -4,7 +4,7 @@ function maxNumber(array) {
     for (let i = 0; i < array.length; i++) {
         let isBigger = true
         for (let j = i + 1; j < array.length; j++) {
-            if (array[i] < array[j]) {
+            if (array[i] <= array[j]) {
                 isBigger = false
                 break;
             }
