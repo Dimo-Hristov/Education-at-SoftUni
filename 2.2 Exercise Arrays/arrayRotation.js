dae8de88aa3ffb7b4719d4fc0099e652c379da57
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-10-01 20:16:31 +0300

    Create arrayRotation.js

---
+++ 2.2 Exercise Arrays/arrayRotation.js
@@ -0,0 +1,19 @@
+function arrayRotation(array, rotations) {
+    // hard way
+    //    for (let j = 0; j < rotations; j++) {
+    //        let firstNum = array[0];
+    //        for (i = 0; i < array.length; i++) {
+    //            array[i] = array[i + 1]
+    //        }
+    //            array[array.length -1] = firstNum
+    //   }
+
+    for (let i = 0; i < rotations; i++) {
+        arrayToRotate = array[0];
+        array.shift()
+        array.push(arrayToRotate)
+    }
+    console.log(array.join(' '));
+
+}
+arrayRotation([51, 47, 32, 61, 21], 2)
