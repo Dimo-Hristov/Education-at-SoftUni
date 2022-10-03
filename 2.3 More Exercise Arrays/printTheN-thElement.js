2d4cfaf4386e6285dedaeb5c34bee7c40dd27c4e
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-10-03 22:35:56 +0300

    update

--- 2.3 More Exercise Arrays/printTheN-thElement.js
+++ 2.3 More Exercise Arrays/printTheN-thElement.js
@@ -1,19 +1,11 @@
-function print(array) {
-    let output = [];
-    let magicNum = Number(array[array.length - 1])
-    let arrayPic = magicNum
-    array[array.length - 1] = []
-
-    for (let i = 0; i <= array.length / 2; i++) {
-        if (i == 0) {
-            output[i] = array[i]
-        } else {
-            output[i] = array[arrayPic]
-            arrayPic += arrayPic
-
+function solve(arr) {
+    let lastNum = arr.pop();
+    let resultArr = [];
+    for (let index in arr) {
+        if (index % lastNum === 0) {
+            resultArr.push(arr[index]);
         }
     }
-    console.log(output.join(' '));
-
+    console.log(resultArr.join(' '));
 }
 print(['5', '20', '31', '4', '20', '2'])
