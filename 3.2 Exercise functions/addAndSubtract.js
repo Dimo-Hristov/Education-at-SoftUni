fc242c500a55ad30e3b23ba7da4e55aa15b4d68e
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-10-08 21:37:59 +0300

    Create addAndSubtract.js

---
+++ 3.2 Exercise functions/addAndSubtract.js
@@ -0,0 +1,9 @@
+function addAndSubtract(num1, num2, num3) {
+
+    let addFirstTwoNumbers = (num1, num2) => num1 + num2;
+
+    let result = addFirstTwoNumbers(num1, num2) - num3
+    console.log(result);
+
+}
+addAndSubtract(23, 6, 10)
