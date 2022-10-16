f21bb7d05c16c1e4b661dd95ad03a25e9b27411e
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-10-16 19:10:57 +0300

    add

--- 4.2 Exercise arrays advanced/6-bombNumbers.js
+++ 4.2 Exercise arrays advanced/6-bombNumbers.js
@@ -18,3 +18,13 @@ function bombNumbers(sequence, bombNumbers) {
 }
 bombNumbers([1, 1, 2, 1, 1, 1, 2, 1, 1, 1],
     [2, 1])
+
+//     6. Bomb Numbers
+
+// Write a function that receives two parameters: sequence of numbers and special bomb number with a certain power.
+
+// Your task is to detonate every occurrence of the special bomb number and according to its power his neighbors from left and right. Detonations are performed from left to right and all detonated numbers disappear.
+
+// The input contains two arrays of numbers. The first contains the initial sequence and the second contains the special bomb number and its power.
+
+// The output is the sum of the remaining elements in the sequence.
