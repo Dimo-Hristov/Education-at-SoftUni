75fbc3beb9303ff6ca6cbdd8e26ba618faf0750e
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-10-27 10:16:00 +0300

    add

---
+++ 1 Fundamentals with JS/7 Objects and classes/1-person-info.js
@@ -0,0 +1,14 @@
+function personInfo(name, lastName, age) {
+    let personInfo = {
+        name: name,
+        lastName: lastName,
+        age: age,
+    }
+
+    console.log(personInfo.name);
+}
+personInfo("Peter",
+
+    "Pan",
+
+    "20")
