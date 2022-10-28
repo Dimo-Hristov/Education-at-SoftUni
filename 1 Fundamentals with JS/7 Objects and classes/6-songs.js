4131973f34111c69a26f452a83fe02131602f232
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-10-28 11:32:09 +0300

    Create 6-songs.js

---
+++ 1 Fundamentals with JS/7 Objects and classes/6-songs.js
@@ -0,0 +1,43 @@
+function songs(input) {
+    class Song {
+        constructor(type, name, time) {
+            this.type = type,
+                this.name = name,
+                this.time = time
+        }
+    }
+
+    let songsCount = Number(input.shift())
+    let typeToPrint = input.pop()
+    let songs = []
+
+
+    for (let i = 0; i < songsCount; i++) {
+        let currentRow = input.shift()
+            .split('_')
+        let type = currentRow[0];
+        let name = currentRow[1];
+        let time = currentRow[2];
+        let currentSong = new Song(type, name, time)
+        songs.push(currentSong)
+    }
+
+    if (typeToPrint == 'all') {
+        songs.forEach(i => console.log(i.type))
+    } else {
+        let filtered = songs.filter(i => i.type == typeToPrint)
+        filtered.forEach(i => console.log(i.name))
+    }
+
+
+
+}
+songs([3,
+
+    'favourite_DownTown_3:14',
+
+    'kur_Kiss_4:16',
+
+    'favourite_Smooth Criminal_4:01',
+
+    'all'])
