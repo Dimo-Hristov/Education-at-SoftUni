46539568011c8f02eb8266e596c7e42d7707c6a0
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-11-03 14:35:32 +0200

    Create 6-word-occurencies.js

---
+++ 1 Fundamentals with JS/8 Associative arrays/6-word-occurencies.js
@@ -0,0 +1,28 @@
+function wordOccurencies(input) {
+    let list = {};
+
+    for (const entry of input) { // take every single word
+        if (list[entry]) { // if the word exist in the list,add 1 to the counter( use the key for counter);
+            list[entry]++
+            continue;
+        }
+        list[entry] = 1 // if doesnt exist add the word to the list with key 1(use key for counter);
+    }
+
+    // sort the the words by repeated times counter, in descending order.
+    let kvpArray = Object.entries(list);
+    kvpArray.sort(([wordA, countA], [wordB, countB]) => countB - countA);
+
+    // print the list 
+
+    kvpArray.forEach(([word, repeatTimes]) =>
+        console.log(`${word} -> ${repeatTimes} times`))
+
+}
+wordOccurencies(["Here", "is", "the", "first", "sentence",
+
+    "Here", "is", "another", "sentence", "And",
+
+    "finally", "the", "third", "sentence"])
+
+    // Write a function that counts the times each word occurs in a text. Print the words sorted by count in descending order. The input comes as an array of strings.
