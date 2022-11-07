d34c94146242b66cd343d82c0c5c6d44700e785a
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-11-07 15:38:52 +0200

    Create 6-company-users.js

---
+++ 1 Fundamentals with JS/8.2 Exercise associative arrays/6-company-users.js
@@ -0,0 +1,46 @@
+function companyUsers(input) {
+    let companyEmployesList = {};
+
+    input.forEach(info => {
+        const [company, employeeId] = info.split(' -> ')
+        if (!companyEmployesList[company]) {
+            companyEmployesList[company] = [employeeId];
+        } else {
+            if (!companyEmployesList[company].includes(employeeId)) {
+                companyEmployesList[company].push(employeeId);
+            }
+        }
+    });
+
+    let kvpArray =
+        Object.entries(companyEmployesList)
+            .sort(([companyA], [companyB]) => companyA.localeCompare(companyB));
+
+    kvpArray.forEach(([companyName, emplyesArr]) => {
+        console.log(companyName);
+        console.log(`-- ${emplyesArr.join('\n-- ')}`);
+    });
+}
+companyUsers(['SoftUni -> AA12345', 'SoftUni -> CC12344', 'Lenovo -> XX23456', 'SoftUni -> AA12345', 'Movement -> DD11111'])
+
+// Write a function, which keeps the information about companies and their employees.
+
+// You will receive an array of strings containing the company name and employee's id. Add each employee to the given company. Keep in mind that a company cannot have two employees with the same id.
+
+// When you finish reading data, order the companies by their name in ascending order.
+
+// Print the company name and each employee's id in the following format:
+
+// {companyName}
+
+// -- {id1}
+
+// -- {id2}
+
+// -- {idN}
+
+// Input / Constraints
+
+// · The input come as array of strings, each in the format: "{companyName} -> {employeeId}".
+
+// · The input always will be valid.
