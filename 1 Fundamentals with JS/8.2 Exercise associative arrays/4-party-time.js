1c21ba99e1472df095a517f5384cebc3fe08768f
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-11-07 15:53:01 +0200

    Update 4-party-time.js
    added condition

--- 1 Fundamentals with JS/8.2 Exercise associative arrays/4-party-time.js
+++ 1 Fundamentals with JS/8.2 Exercise associative arrays/4-party-time.js
@@ -38,3 +38,17 @@ function partyTime(input) {
 }
 partyTime(['7IK9Yo0h', '9NoBUajQ', 'Ce8vwPmE', 'SVQXQCbc',
     'tSzE5t0p', 'PARTY', '9NoBUajQ', 'Ce8vwPmE', 'SVQXQCbc'])
+
+//     4. Party Time
+
+// There is a party at SoftUni. Many guests are invited and they are two types: VIP and regular. When guests come to the party check if he/she contains in any of the two reservation lists.
+
+// The input will come as an array of strings. You will be given the list with the guests before you receive a command "PARTY".
+
+// All VIP numbers start with a digit.
+
+// When you receive the command "PARTY", the guests start coming.
+
+// Print the count of guests then all guests, who didn't come to the party (VIP must be printed first).
+
+// Hint: Guest names are not unique. Only the first match is removed when receiving a name.
