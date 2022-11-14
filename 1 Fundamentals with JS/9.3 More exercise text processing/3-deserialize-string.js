dd319c3e36ac5eeb1b07106046f1a48e90215d9f
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-11-15 00:06:49 +0200

    Update 3

--- 1 Fundamentals with JS/9.3 More exercise text processing/3-deserialize-string.js
+++ 1 Fundamentals with JS/9.3 More exercise text processing/3-deserialize-string.js
@@ -27,3 +27,11 @@ deserializeString([
     "l:12",
     "end",
 ]);
+
+// Write a function, which takes the output from the previous task and turns it back into a string.
+
+// Until you receive the line “end”, you will receive several lines of input on the console, in the following format:
+
+// · "{letter}:{index1}/{index2}/{index…}/{indexN}"
+
+// Your task is to take every letter and its index and form a string out of them.
