93a585de93e255337402f691af6b295a891aec69
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2022-11-23 13:22:44 +0100

    Create 9-destination-mapper.js

---
+++ 1 Fundamentals with JS/Final exam preparation/9-destination-mapper.js
@@ -0,0 +1,33 @@
+function destinationMapper(text) {
+    let pattern = /([\/=]{1})(?<destination>[A-Z]{1}[A-Za-z]{2,})\1/gm;
+
+    let matches = text.matchAll(pattern);
+
+    let travelPoints = 0;
+    let destinationsArr = [];
+
+    for (const match of matches) {
+        destinationsArr.push(match.groups.destination);
+        travelPoints += match.groups.destination.length;
+    }
+
+    console.log(`Destinations: ${destinationsArr.join(', ')}`);
+    console.log(`Travel Points: ${travelPoints}`);
+
+}
+destinationMapper("=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=");
+
+// Now that you have planned out your tour, you are ready to go! Your next task is to mark all the points on the map that you are going to visit.
+// You will be given a string representing some places on the map. You have to filter only the valid ones. A valid location is:
+// •	Surrounded by "=" or "/" on both sides (the first and the last symbols must match)
+// •	After the first "=" or "/" there should be only letters (the first must be upper-case, other letters could be upper or lower-case)
+// •	The letters must be at least 3
+// Example: In the string "=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=" only the first two locations are valid.
+// After you have matched all the valid locations, you have to calculate travel points. They are calculated by summing the lengths of all the valid destinations that you have found on the map.
+// In the end, on the first line, print: "Destinations: {destinations joined by ', '}".
+// On the second line, print "Travel Points: {travel_points}".
+// Input / Constraints
+// •	You will receive a string representing the locations on the map
+// •	JavaScript: you will receive a single parameter: string
+// Output
+// •	Print the messages described above
