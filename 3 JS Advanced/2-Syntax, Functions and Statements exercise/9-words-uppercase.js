7409c6c1c20a4e86a5417afaa589a7abec2af017
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-01-14 19:40:36 +0100

    Create 9-words-uppercase.js

---
+++ 3 JS Advanced/2-Syntax, Functions and Statements exercise/9-words-uppercase.js
@@ -0,0 +1,21 @@
+function wordsUppercase(input) {
+    const pattern = /\w+/gm;
+    const words = input.matchAll(pattern);
+    const output = [];
+
+    for (const iterator of words) {
+        const currentWord = iterator[0].toUpperCase()
+        output.push(currentWord);
+    }
+
+    console.log(output.join(', '));
+
+}
+wordsUppercase('Hi, how are you?');
+
+
+// Write a program that extracts all words from a passed-in string and converts them to upper case. The extracted words in the upper case must be printed on a single line separated by ", ".
+
+// The input comes as a single string argument - the text to extract and convert words from.
+
+// The output should be a single line containing the converted string.
