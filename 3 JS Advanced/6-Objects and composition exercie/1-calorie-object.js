9f62d5ccd666934ee8de2406c58f49d8030b6e89
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-01-19 23:03:03 +0100

    update 1-calorie-object.js
    add description

--- 3 JS Advanced/6-Objects and composition exercie/1-calorie-object.js
+++ 3 JS Advanced/6-Objects and composition exercie/1-calorie-object.js
@@ -1,13 +1,16 @@
 function calorieObject(arr) {
     return arr.reduce((acc, element, index) => {
-        if (index % 2 === 0) {
-            acc[element] = Number(arr[index + 1]);
-            return acc
-        }
-
+        if (index % 2 === 0) acc[element] = Number(arr[index + 1]);
+        return acc;
 
     }, {})
 }
 console.log(calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']));
 console.log('------------');
 console.log(calorieObject(['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42']));
+
+// Write a function that composes an object by given properties. The input comes as an array of strings. Every even index of the array represents the name of the food. Every odd index is a number that is equal to the calories in 100 grams of the given product. Assign each value to its corresponding property, and finally print the object.
+
+// The input comes as an array of string elements.
+
+// The output should be printed on the console.
