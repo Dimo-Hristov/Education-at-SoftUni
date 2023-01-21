7af713cf40716e2da17c56a74d0f530035987247
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-01-21 23:15:21 +0100

    complete task 2 CollectListitems

---
+++ 3 JS Advanced/7 DOM introduction/02.Collect List Items/CollectListItems.js
@@ -0,0 +1,12 @@
+function extractText() {
+    let itemNodes =
+        document.querySelectorAll("ul#items li");
+
+    let textarea =
+        document.querySelector("#result");
+
+    for (let node of itemNodes) {
+        textarea.value += node.textContent + "\n";
+    }
+
+}
