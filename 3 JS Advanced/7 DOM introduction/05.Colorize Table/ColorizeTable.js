ea8fe57be44e2e52b9ecd1a2243063724c81bced
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-01-21 23:33:18 +0100

    complete task 5 colorizeTable

---
+++ 3 JS Advanced/7 DOM introduction/05.Colorize Table/ColorizeTable.js
@@ -0,0 +1,10 @@
+function colorize() {
+    let rows = document.querySelectorAll('table tr');
+    let index = 0;
+    for (const row of rows) {
+        index++
+        if (index % 2 == 0) {
+            row.style.background = 'teal';
+        }
+    }
+}
