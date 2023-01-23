7830fbb6a50d11be849d3471c68b15a9014c096f
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-01-23 22:34:06 +0100

    Update subtract.js

--- 3 JS Advanced/8-Dom insrodution Exercise/01. Subtraction/subtract.js
+++ 3 JS Advanced/8-Dom insrodution Exercise/01. Subtraction/subtract.js
@@ -8,3 +8,13 @@ function subtract() {
     let result = firstNumber - secondNumber;
     document.getElementById("result").textContent = result;
 }
+
+// An HTML page holds two text fields with ids "firstNumber" and "secondNumber". Write a function to subtract the values from these text fields and display the result in the div named "result".
+
+// HTML and JavaScript Code
+
+// Implement the above to provide the following functionality:
+
+// · Your function should take the values of "firstNumber" and "secondNumber", convert them to numbers, subtract the first number from the second one and then append the result to the <div> with id="result".
+
+// · Your function should be able to work with any 2 numbers in the inputs, not only the ones given in the example.
