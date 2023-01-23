c3f738d9eac638adcb17b840782b8079a4a9f994
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-01-23 23:04:28 +0100

    complete task accordion.js

--- 3 JS Advanced/8-Dom insrodution Exercise/03. Accordion/accordion.js
+++ 3 JS Advanced/8-Dom insrodution Exercise/03. Accordion/accordion.js
@@ -1,3 +1,11 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonElement = document.querySelector('.button');
+
+    if (buttonElement.textContent == 'More') {
+        buttonElement.textContent = 'Less';
+        document.querySelector('#extra').style.display = 'block';
+    } else {
+        buttonElement.textContent = 'More';
+        document.querySelector('#extra').style.display = 'none';
+    }
 }
