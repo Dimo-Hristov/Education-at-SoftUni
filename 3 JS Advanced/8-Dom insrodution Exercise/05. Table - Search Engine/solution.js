1462cfbc474a7c7ac5daf4f796a086e7f8d4f6c0
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-01-24 00:59:47 +0100

    Update solution.js

--- 3 JS Advanced/8-Dom insrodution Exercise/05. Table - Search Engine/solution.js
+++ 3 JS Advanced/8-Dom insrodution Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,25 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let input = document.getElementById('searchField');
+      let rows = document.querySelectorAll('tbody tr');
 
+      for (const row of rows) {
+         console.log(row.textContent);
+         if (input.value !== '' && row.textContent.includes(input.value)) {
+            row.className = 'select'
+         } else {
+            row.className = ''
+         }
+      }
    }
 }
+
+// Write a function that searches in a table by given input.
+// When the "Search" button is clicked, go through all cells in the table except for the first row (Student name, Student email, and Student course) and check if the given input has a match (check for both full words and single letters).
+
+// If any of the rows contain the submitted string, add a class select to that row. Note that more than one row may contain the given string.
+
+// Оtherwise, if there is no match, nothing should happen.
+
+// Note: After every search ("Search" button is clicked), clear the input field and remove all already selected classes (if any) from the previous search, for the new search to contain only the new result.
