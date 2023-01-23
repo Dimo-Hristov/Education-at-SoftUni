b8a5c01154fb9473365bc9b0a3e4b994f5a9f594
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-01-23 21:33:04 +0100

    complete task 1 substration

--- 3 JS Advanced/8-Dom insrodution Exercise/01. Subtraction/subtract.js
+++ 3 JS Advanced/8-Dom insrodution Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,10 @@
 function subtract() {
-    console.log('TODO:...');
+    let firstElement = document.getElementById("firstNumber");
+    let secondElement = document.getElementById("secondNumber");
+
+    let firstNumber = Number(firstElement.value);
+    let secondNumber = Number(secondElement.value);
+
+    let result = firstNumber - secondNumber;
+    document.getElementById("result").textContent = result;
 }
