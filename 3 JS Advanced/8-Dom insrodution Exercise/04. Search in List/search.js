7933ff848b29500bdaae5d260a572534e9616c61
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-01-24 00:09:36 +0100

    Update search.js

--- 3 JS Advanced/8-Dom insrodution Exercise/04. Search in List/search.js
+++ 3 JS Advanced/8-Dom insrodution Exercise/04. Search in List/search.js
@@ -1,3 +1,23 @@
 function search() {
-   // TODO
+   let givenLetters = document.getElementById('searchText').value;
+   let townsElement = Array.from(document.querySelectorAll('#towns li'));
+   let counter = 0;
+
+   for (const town of townsElement) {
+      if (town.textContent.includes(givenLetters)) {
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline'
+         counter++
+      } else {
+         town.style.fontWeight = 'normal';
+         town.style.textDecoration = 'none';
+      }
+   }
+   document.getElementById('result').textContent = `${counter} matches found`
 }
+
+// An HTML page holds a list of towns, a search box, and a [Search] button. Implement the search function to bold and underline the items from the list which include the text from the search box. Also, print the number of items the current search matches in the format `${matches} matches found`.
+
+// Note: It is necessary to clear the results of the previous search.
+
+// Write your JavaScript code in this file:
