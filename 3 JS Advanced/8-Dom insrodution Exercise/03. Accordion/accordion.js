4a88d2eecc20586057531999edb4f8843df07fac
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-01-24 00:10:02 +0100

    Update accordion.js

--- 3 JS Advanced/8-Dom insrodution Exercise/03. Accordion/accordion.js
+++ 3 JS Advanced/8-Dom insrodution Exercise/03. Accordion/accordion.js
@@ -9,3 +9,5 @@ function toggle() {
         document.querySelector('#extra').style.display = 'none';
     }
 }
+
+// An HTML file is given and your task is to show more/less information. By clicking the [More] button, it should reveal the content of a hidden div and changes the text of the button to [Less]. When the same link is clicked again (now reading Less), hide the div and change the text of the link to More. Link action should be toggleable (you should be able to click the button an infinite amount of times).
