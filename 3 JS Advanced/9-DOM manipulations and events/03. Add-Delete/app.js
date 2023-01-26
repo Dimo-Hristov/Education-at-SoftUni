da8e18a12cb33977f8bb55cdd510790ecaa96bfa
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-01-26 15:39:39 +0100

    Update app.js

--- 3 JS Advanced/9-DOM manipulations and events/03. Add-Delete/app.js
+++ 3 JS Advanced/9-DOM manipulations and events/03. Add-Delete/app.js
@@ -1,3 +1,22 @@
 function addItem() {
-    //TODO...
+    let newItemElement = document.getElementById('newItemText');
+    let ulElement = document.getElementById('items');
+
+    let newLiElement = document.createElement('li');
+    newLiElement.textContent = newItemElement.value;
+    newItemElement.value = ''
+
+    let deleteElement = document.createElement('a');
+    deleteElement.href = '#';
+    deleteElement.textContent = '[Delete]';
+
+    deleteElement.addEventListener('click', (e) => {
+        console.log(e.currentTarget.parentElement.remove());
+    });
+
+    newLiElement.appendChild(deleteElement)
+    ulElement.appendChild(newLiElement);
 }
+
+
+// Extend the previous problem to display a [Delete] link after each list item. Clicking it should delete the item with no confirmation. You have to add href="#" to the link element.
