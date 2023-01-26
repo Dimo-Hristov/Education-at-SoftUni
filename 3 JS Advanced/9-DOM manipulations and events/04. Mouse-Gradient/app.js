494cfaf461ed5c46b805daaccc22420cdcd9a62b
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-01-26 16:04:04 +0100

    Update app.js

--- 3 JS Advanced/9-DOM manipulations and events/04. Mouse-Gradient/app.js
+++ 3 JS Advanced/9-DOM manipulations and events/04. Mouse-Gradient/app.js
@@ -1,3 +1,16 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientElement = document.getElementById('gradient-box');
+    gradientElement.addEventListener('mousemove', (e) => {
+        document.getElementById('result').textContent = `${Math.floor(e.offsetX / 300 * 100)}%`;
+    })
 }
+
+// 4. Mouse Gradient
+
+// Write a program that detects and displays how far along a gradient the user has moved their mouse. The result should be rounded down and displayed as a percentage inside the <div> with id "result".
+
+// Submit only the attachGradientEvents() function in Judge.
+
+// Input/Output
+
+// There will be no input/output, your program should instead modify the DOM of the given HTML document.
