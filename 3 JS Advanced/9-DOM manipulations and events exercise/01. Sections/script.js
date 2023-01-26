5a4c2dc7cdc513f659372f6280a5cf0f8df9eca4
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-01-26 22:21:47 +0100

    Update script.js

--- 3 JS Advanced/9-DOM manipulations and events exercise/01. Sections/script.js
+++ 3 JS Advanced/9-DOM manipulations and events exercise/01. Sections/script.js
@@ -1,3 +1,18 @@
 function create(words) {
-   console.log('TODO:...');
+   let parent = document.getElementById("content");
+
+   for (const word of words) {
+      let div = document.createElement("div");
+      let p = document.createElement("p");
+      p.style.display = "none";
+      p.textContent = word;
+      div.appendChild(p);
+      div.addEventListener("click", (e) => {
+         e.target.querySelector("p").style.display = "block";
+      });
+      parent.appendChild(div);
+   }
 }
+
+
+// You will receive an array of strings. For each string, create a div with a paragraph with the string in it. Each paragraph is initially hidden (display:none). Add a click event listener to each div that displays the hidden paragraph. Finally, you should append all divs to the element with an id "content".
