cd95552a5e58a21dcc1e01415038adfe1a071b8a
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-01-26 14:03:42 +0100

    Update app.js

--- 3 JS Advanced/9-DOM manipulations and events/01. List-Of-Items/app.js
+++ 3 JS Advanced/9-DOM manipulations and events/01. List-Of-Items/app.js
@@ -1,3 +1,10 @@
 function addItem() {
-    console.log('TODO:...');
+    let ulElement = document.getElementById('items');
+    let newTextElement = document.getElementById('newItemText');
+    let newElement = document.createElement('li');
+    newElement.textContent = newTextElement.value;
+    ulElement.appendChild(newElement);
+    newTextElement.value = ''
 }
+
+// Write a function that reads the text inside an input field and appends the specified text to a list inside an HTML page.
