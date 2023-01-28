4117063713937d864d889580ff74a1132c3b3698
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-01-28 20:19:50 +0100

    1-areaAndvolumeCalculator.js
    Solve it with fancy code.

--- 3 JS Advanced/10-Advanced functions/1-areaAndvolumeCalculator.js
+++ 3 JS Advanced/10-Advanced functions/1-areaAndvolumeCalculator.js
@@ -10,26 +10,37 @@ function vol() {
 
 };
 
+// function solve(area, vol, input) {
+//     const cordinates = JSON.parse(input);
+//     const result = [];
+//     for (const point of cordinates) {
+//         let obj = {
+//             area: area.call(point),
+//             volume: vol.call(point),
+//         }
+//         result.push(obj);
+//     }
+//     return result;
+// }
+
+// Fancy solve 
 function solve(area, vol, input) {
-    const cordinates = JSON.parse(input);
-    const result = [];
-    for (const point of cordinates) {
-        let obj = {
-            area: area.call(point),
-            volume: vol.call(point),
-        }
-        result.push(obj);
-    }
-    return result;
+return JSON.parse(input).map( x => ({
+            area: area.call(x),
+            volume: vol.call(x),
+        })
+        );
+    
 }
+
 console.log(solve(area, vol, `[
     {"x":"1","y":"2","z":"10"},  
     {"x":"7","y":"7","z":"10"}, 
     {"x":"5","y":"2","z":"10"} 
     ]`));
 
-    Write a function that calculates the area and the volume of a figure, which is defined by its coordinates (x, y, z).
-    Input
+    // Write a function that calculates the area and the volume of a figure, which is defined by its coordinates (x, y, z).
+    // Input
 
 // You will receive 3 parameters - the functions area and vol and a string, which contains the figures' coordinates.
 
@@ -54,7 +65,8 @@ console.log(solve(area, vol, `[
 // Submit only the solve function.
 
 
-// INPUT 
+// INPUT =>>>>
+
 // area, vol, `[
 
 //     {"x":"1","y":"2","z":"10"},
@@ -65,5 +77,6 @@ console.log(solve(area, vol, `[
     
 //     ]
 
-//     OUTPUT 
+//     OUTPUT =>>>>>
+
 //     [ { area: 2, volume: 20 }, { area: 49, volume: 490 }, { area: 10, volume: 100 } ]
