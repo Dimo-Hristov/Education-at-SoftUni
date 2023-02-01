0099faf99a9fad55404bada1a0451f57647c4d7a
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-02-01 22:01:22 +0100

    Create 4-sum-of-number.js

---
+++ 3 JS Advanced/12-Unit testing and error handling/4-sum-of-number.js
@@ -0,0 +1,20 @@
+function sum(arr) {
+
+    let sum = 0;
+
+    for (let num of arr) {
+
+        sum += Number(num);
+
+    }
+
+    return sum;
+}
+
+//     Write tests to check the functionality of the following code:
+
+//     Your tests will be supplied with a function named 'sum()'. It should meet the following requirements:
+
+// · Take an array of numbers as an argument
+
+// · Return the sum of the values of all elements inside the array
