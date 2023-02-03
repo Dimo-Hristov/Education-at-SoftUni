a8312c8d2cfa2936373ed4be5c08feccd30025a9
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-02-03 11:08:34 +0100

    complete task 6

--- 3 JS Advanced/13-Unit testing and error handling exercise/06.Dynamic-Validation/app.js
+++ 3 JS Advanced/13-Unit testing and error handling exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    // TODO
+    const emailValidator = /^[a-z-\.]+@[a-z-\.]+\.[a-z]{2,4}/
+    const inputElement = document.getElementById('email');
+
+    inputElement.addEventListener('change', () => {
+        if (inputElement.value.match(emailValidator)) {
+            inputElement.classList.remove('error');
+        } else {
+            inputElement.classList.add('error');
+        }
+    })
 }
