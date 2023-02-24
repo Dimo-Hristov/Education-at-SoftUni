e9e0b9720d65defe7f5ae4a8652348e0592fc524
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-02-24 12:26:36 +0100

    make http request and get the info from url

--- 4 JS Applications/02 Asynchronys programming/01.XHR/scripts/app.js
+++ 4 JS Applications/02 Asynchronys programming/01.XHR/scripts/app.js
@@ -1,3 +1,15 @@
 function loadRepos() {
-   console.log("TODO...");
+   const resultElement = document.getElementById('res');
+
+   let url = 'https://api.github.com/users/testnakov/repos';
+   const httpRequest = new XMLHttpRequest();
+
+   httpRequest.addEventListener('readystatechange', () => {
+      if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+         resultElement.textContent = httpRequest.responseText;
+      }
+   })
+
+   httpRequest.open('GET', url);
+   httpRequest.send();
 }
