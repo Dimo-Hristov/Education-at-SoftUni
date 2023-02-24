affa474db86fbf0f8ddd79e229a1463bc5f8a869
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-02-24 20:46:55 +0100

    Update app.js

--- 4 JS Applications/02 Asynchronys programming/03.Gighub-Commits/app.js
+++ 4 JS Applications/02 Asynchronys programming/03.Gighub-Commits/app.js
@@ -1,4 +1,34 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const usernameElement = document.getElementById('username');
+    const username = usernameElement.value;
+    const repositoryElement = document.getElementById('repo');
+    const repository = repositoryElement.value;
+
+    const ulElement = document.getElementById('commits');
+    ulElement.innerHTML = '';
+    const url = 'https://api.github.com/repos';
+
+    fetch(`${url}/${username}/${repository}/commits`)
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach(x => {
+                const content = `${x.commit.author.name}: ${x.commit.message}`
+                elementCreator('li', content, ulElement)
+            })
+        })
+        .catch((error) => {
+            const content = `Error: ${error.status} (Not Found)`
+            elementCreator('li', content, ulElement)
+        })
+
+}
+
+function elementCreator(type, content, parent) {
+    let element = document.createElement(type);
+    element.textContent = content;
+
+    if (parent) {
+        parent.appendChild(element)
+    }
+    return element
 }
