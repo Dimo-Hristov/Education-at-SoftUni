4b9325c28c357e78f43cf36c8cb7a752e377d38c
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-02-24 13:23:31 +0100

    load user repositories using fetch API

--- 4 JS Applications/02 Asynchronys programming/02.Github-Repos/scripts/app.js
+++ 4 JS Applications/02 Asynchronys programming/02.Github-Repos/scripts/app.js
@@ -1,3 +1,33 @@
 function loadRepos() {
-	console.log("TODO...");
+	const url = 'https://api.github.com/users/';
+	const inputElement = document.getElementById('username');
+	const username = inputElement.value;
+	const ulElement = document.getElementById('repos');
+
+	ulElement.innerHTML = '';
+
+	fetch(`${url}${username}/repos`)
+		.then((response) => response.json())
+		.then((data) => {
+			data.forEach(x => {
+				const listElement = elementCreator('li', '', ulElement);
+
+				const anchorTag = elementCreator('a', x.full_name, listElement)
+				anchorTag.setAttribute('href', x.html_url);
+				anchorTag.setAttribute('target', '_blank');
+			});
+
+		})
+		.catch((error) => console.log(error));
+
+}
+
+function elementCreator(type, content, parent) {
+	let element = document.createElement(type);
+	element.textContent = content;
+
+	if (parent) {
+		parent.appendChild(element)
+	}
+	return element
 }
