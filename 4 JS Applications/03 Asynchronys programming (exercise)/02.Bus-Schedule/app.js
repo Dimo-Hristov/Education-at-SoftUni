39f9e7afa1cbcba13a5b60d61e6544016303db01
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-02-26 22:21:00 +0100

    Update app.js

--- 4 JS Applications/03 Asynchronys programming (exercise)/02.Bus-Schedule/app.js
+++ 4 JS Applications/03 Asynchronys programming (exercise)/02.Bus-Schedule/app.js
@@ -1,11 +1,34 @@
 function solve() {
+    const messageArea = document.querySelector('.info');
+    const departButton = document.getElementById('depart');
+    const arriveButton = document.getElementById('arrive');
+    let firstStop = 'depot';
+    let secondStop = '';
+    const url = 'http://localhost:3030/jsonstore/bus/schedule/';
 
     function depart() {
-        console.log('Depart TODO...');
+
+        fetch(`${url}${firstStop}`)
+            .then((response) => response.json())
+            .then((data) => {
+                firstStop = data.name;
+                secondStop = data.next;
+                messageArea.textContent = `Next stop ${firstStop}`
+                departButton.disabled = true;
+                arriveButton.disabled = false;
+            })
+            .catch((error) => {
+                messageArea.textContent = 'Error';
+                departButton.disabled = true;
+                arriveButton.disabled = true;
+            })
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        messageArea.textContent = `Arriving at ${firstStop}`
+        firstStop = secondStop;
+        departButton.disabled = false;
+        arriveButton.disabled = true;
     }
 
     return {
