0e332a47370b4af7047ed0e5b78e2e2dc4b12f79
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-03-02 20:23:40 +0100

    make load button to load all movies from server

--- 4 JS Applications/04 Remote data and authentication/04.Book-library/app.js
+++ 4 JS Applications/04 Remote data and authentication/04.Book-library/app.js
@@ -1,1 +1,47 @@
-console.log('My requests...')
+function solve() {
+    const loadBtn = document.getElementById('loadBooks');
+    const baseUrl = 'http://localhost:3030/jsonstore/collections/books';
+    const tBody = document.querySelector('table tbody')
+
+    loadBtn.addEventListener('click', loadAllBooks)
+
+
+
+    function loadAllBooks() {
+
+        fetch(baseUrl)
+            .then(res => {
+                if (!res.ok) alert('Response error')
+                return res.json()
+            })
+            .then(data => {
+                Object.values(data).forEach(x => {
+                    const tableRow = document.createElement('tr');
+
+                    const titleCell = tableRow.insertCell(0);
+                    titleCell.textContent = x.title;
+
+                    const authorCell = tableRow.insertCell(1);
+                    authorCell.textContent = x.author;
+
+                    const buttonsTd = tableRow.insertCell(2);
+
+                    const editBtn = document.createElement('button');
+                    editBtn.textContent = 'Edit';
+                    buttonsTd.appendChild(editBtn);
+
+                    const deleteBtn = document.createElement('button')
+                    deleteBtn.textContent = 'Delete';
+                    buttonsTd.appendChild(deleteBtn);
+
+
+
+
+
+                    tBody.appendChild(tableRow)
+                })
+
+            })
+    }
+}
+solve()
