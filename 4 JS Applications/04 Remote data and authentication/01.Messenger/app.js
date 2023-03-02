946feee8bf3f07242eb5544407b3596b5acc3955
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-03-02 09:41:12 +0100

    make reshresh button show all comments

--- 4 JS Applications/04 Remote data and authentication/01.Messenger/app.js
+++ 4 JS Applications/04 Remote data and authentication/01.Messenger/app.js
@@ -1,5 +1,28 @@
 function attachEvents() {
-    console.log('TODO...');
+    const sendBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+    const textAreaElement = document.getElementById('messages');
+    const url = 'http://localhost:3030/jsonstore/messenger';
+
+    refreshBtn.addEventListener('click', () => {
+        textAreaElement.innerHTML = '';
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                const comments = [];
+
+                Object.values(data).forEach(x => {
+                    comments.push(`${x.author}: ${x.content}`)
+                })
+                textAreaElement.textContent = comments.join('\n')
+            })
+            .catch(error => alert(error.message))
+    })
+
+    sendBtn.addEventListener('click', () => {
+
+    })
 }
 
 attachEvents();
