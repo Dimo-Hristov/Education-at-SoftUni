9ab2631d75e5812082e1fd3cd66a5867116f0af3
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-03-02 12:05:25 +0100

    load table with students on enter

--- 4 JS Applications/04 Remote data and authentication/03.Students/app.js
+++ 4 JS Applications/04 Remote data and authentication/03.Students/app.js
@@ -1,1 +1,31 @@
-console.log('TODO...');
+function solve() {
+    const tableBodyElement = document.querySelector('#results tbody');
+    const url = 'http://localhost:3030/jsonstore/collections/students';
+
+    fetch(url)
+        .then(res => {
+            if (!res.ok) throw new Error('Error')
+            return res.json()
+        })
+        .then(data => {
+            Object.values(data).forEach(x => {
+                const tableRowElement = document.createElement('tr');
+
+                const firstNameCell = tableRowElement.insertCell(0);
+                firstNameCell.textContent = x.firstName;
+
+                const lastnameCell = tableRowElement.insertCell(1);
+                lastnameCell.textContent = x.lastName;
+
+                const facultyNumber = tableRowElement.insertCell(2);
+                facultyNumber.textContent = x.facultyNumber;
+
+                const grade = tableRowElement.insertCell(3);
+                grade.textContent = x.grade
+                console.log(x);
+                tableBodyElement.appendChild(tableRowElement)
+            })
+        })
+
+}
+solve()
