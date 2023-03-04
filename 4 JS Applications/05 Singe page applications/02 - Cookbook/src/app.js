c1d29e238aecbe658f39fdaa44569e2bdd3780dc
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-03-04 21:00:39 +0100

    get navigation then button is clicked

--- 4 JS Applications/05 Singe page applications/02 - Cookbook/src/app.js
+++ 4 JS Applications/05 Singe page applications/02 - Cookbook/src/app.js
@@ -1,19 +1,17 @@
-import { router } from './router.js';
-import { updateAuth } from './auth.js';
+const homeSection = document.querySelector('.home');
+homeSection.style.display = 'block';
 
-updateAuth();
-router('/');
+const guestNavigation = document.getElementById('guest');
+const userNavigation = document.getElementById('user');
+guestNavigation.style.display = 'inline';
+userNavigation.style.display = 'inline'
 
 const navigationElement = document.querySelector('.navigation');
 navigationElement.addEventListener('click', (e) => {
-    e.preventDefault();
-
+    e.preventDefault()
     if (e.target.tagName == 'A') {
-        let url = new URL(e.target.href);
-    
-        document.querySelector('.active').classList.remove('active');
-        e.target.classList.add('active');
+        const url = new URL(e.target.href);
 
-        router(url.pathname);
+        console.log(url.pathname);
     }
-});
+})
