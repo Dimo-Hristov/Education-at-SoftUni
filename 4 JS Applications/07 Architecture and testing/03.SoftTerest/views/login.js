ec1cc6a1f353aa3343140a8f4fcfb4879c2c8ef0
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-03-10 12:46:07 +0100

    add login functionality

--- 4 JS Applications/07 Architecture and testing/03.SoftTerest/views/login.js
+++ 4 JS Applications/07 Architecture and testing/03.SoftTerest/views/login.js
@@ -1,7 +1,23 @@
-
+import { login } from '../src/api/users.js'
 
 const section = document.getElementById('loginPage');
+const form = section.querySelector('form');
+form.addEventListener('submit', onSubmit);
+
+let ctx = null;
 
 export function showLogin(context) {
+    ctx = context;
     context.showSection(section)
+}
+
+async function onSubmit(e) {
+    e.preventDefault();
+    const formdata = new FormData(form);
+
+    const email = formdata.get('email');
+    const password = formdata.get('password');
+
+    await login(email, password);
+    ctx.goTo('/')
 }
