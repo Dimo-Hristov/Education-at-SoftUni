99ee3044bb09a90de37dde3f6536a8fc133c08c3
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-03-10 12:46:23 +0100

    add register functionality

--- 4 JS Applications/07 Architecture and testing/03.SoftTerest/views/register.js
+++ 4 JS Applications/07 Architecture and testing/03.SoftTerest/views/register.js
@@ -1,6 +1,23 @@
+import { register } from "../src/api/users.js";
 
 const section = document.getElementById('registerPage');
+const form = section.querySelector('form');
+form.addEventListener('submit', onSubmit);
+
+let ctx = null;
 
 export function showRegister(context) {
+    ctx = context;
     context.showSection(section)
+}
+
+async function onSubmit(e) {
+    e.preventDefault();
+    const formdata = new FormData(form);
+
+    const email = formdata.get('email');
+    const password = formdata.get('password');
+
+    await register(email, password);
+    ctx.goTo('/')
 }
