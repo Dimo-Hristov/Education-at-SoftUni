1a4760fb5bf3254a83385d48068edca8834b8e84
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-03-12 13:27:39 +0100

    upgrade
    Change Show more button position

--- 4 JS Applications/08 Client side rendering/02.HTTP-Status-Cats/app.js
+++ 4 JS Applications/08 Client side rendering/02.HTTP-Status-Cats/app.js
@@ -9,11 +9,11 @@ const template = (cats) => html`
     <li>
         <img src="./images/${x.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
         <div class="info">
-            <button class="showBtn">Show status code</button>
             <div class="status" style="display: none" id="100">
                 <h4>Status Code: ${x.statusCode}</h4>
                 <p>${x.statusMessage}</p>
             </div>
+            <button class="showBtn">Show status code</button>
         </div>
     </li>`)}
 </ul>`;
--- 4 JS Applications/08 Client side rendering/03.Search-in-List/search.js
+++ 4 JS Applications/08 Client side rendering/03.Search-in-List/search.js
@@ -1,3 +1,6 @@
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+import { towns } from './towns.js';
+
 function search() {
-   // TODO
+
 }
