fbb306427bd78b088f361c4bae65e49480ea324d
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-03-12 12:10:44 +0100

    upload task files

--- 4 JS Applications/08 Client side rendering/01.Contacts/app.js
+++
@@ -1,9 +0,0 @@
-import { html, render } from '../node_modules/lit-html/lit-html.js';
-
-const mainDiv = document.getElementById('contacts');
-
-const name = 'Dimo'
-
-
-let sayHello = (name) => html`<h1>Hello ${name}</h1>`;
-render(sayHello('world'), mainDiv)
