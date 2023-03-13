5e99feb6996e8f2443b958427d66b58418ec9187
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-03-13 10:37:04 +0100

    complete task 3

--- 4 JS Applications/08 Client side rendering/03.Search-in-List/search.js
+++ 4 JS Applications/08 Client side rendering/03.Search-in-List/search.js
@@ -1,6 +1,26 @@
 import { html, render } from '../node_modules/lit-html/lit-html.js';
 import { towns } from './towns.js';
+const resultDiv = document.getElementById('result');
+const townsDiv = document.getElementById('towns');
+document.querySelector('button').addEventListener('click', search);
+
 
 function search() {
+   const searchText = document.getElementById('searchText');
 
+   const template = (towns) => html`
+   <ul>
+      ${towns.map(x => html`<li class=${searchText.value != '' && x.includes(searchText.value) ? "active"  : "" }>${x}</li>`)}
+   </ul>`;
+
+   render(template(towns), townsDiv);
+   resultDiv.textContent = `${counter()} matches found`;
 }
+search();
+
+function counter() {
+   let matchesQty = townsDiv.querySelectorAll('.active');
+
+   return matchesQty.length || 'No'
+}
+
