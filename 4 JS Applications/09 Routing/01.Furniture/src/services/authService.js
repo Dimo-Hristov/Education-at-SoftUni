6bab50f5dc66e13755c7b34cb470a91c05f9eabe
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-03-14 22:40:27 +0100

    login to the server done

---
+++ 4 JS Applications/09 Routing/01.Furniture/src/services/authService.js
@@ -0,0 +1,34 @@
+const baseUrl = "http://localhost:3030/users";
+
+// accessToken: "53d36db4a82235324905444016df06361666aca3cf8c3253bae36a47cad30e8a";
+// email: "peter@abv.bg";
+// username: "Peter";
+// _id: "35c62d76-8152-4626-8712-eeb96381bea8";
+
+const save = (user) => {
+    if (user) {
+        localStorage.setItem("accessToken", user.accessToken);
+        localStorage.setItem("email", user.email);
+        localStorage.setItem("username", user.username);
+        localStorage.setItem("_id", user._id);
+    }
+};
+
+export const login = (email, password) => {
+    return fetch(`${baseUrl}/login`, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+        },
+        body: JSON.stringify({
+            email,
+            password,
+        }),
+    })
+        .then((res) => res.json())
+        .then((user) => {
+            save(user);
+            return user
+        })
+        .catch(error => alert(error.message))
+};
--- 4 JS Applications/09 Routing/01.Furniture/src/views/loginView.js
+++ 4 JS Applications/09 Routing/01.Furniture/src/views/loginView.js
@@ -1,9 +1,35 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
+import * as authService from '../services/authService.js'
 
-const loginTemplate = () => html`
-<h1>Login Page</h1>
+const loginTemplate = (loginHandler) => html`
+<div class="login-container">
+    <h1>Login Page</h1>
+    <form @submit=${loginHandler}>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
 `
 
 export const loginView = (ctx) => {
-    ctx.render(loginTemplate())
+
+    const loginHandler = (e) => {
+        e.preventDefault();
+        let formData = new FormData(e.currentTarget);
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        authService.login(email, password)
+            .then(user => alert(`Successfully logged in with user: ${user.username}`))
+    }
+
+    ctx.render(loginTemplate(loginHandler))
 }
