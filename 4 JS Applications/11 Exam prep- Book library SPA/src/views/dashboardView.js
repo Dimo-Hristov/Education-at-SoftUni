414f7387b960dca894c5c5386a2090b58df02db9
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-03-16 22:02:44 +0100

    start from the begining

--- 4 JS Applications/11 Exam prep- Book library SPA/src/views/dashboardView.js
+++
@@ -1,9 +0,0 @@
-import { html } from "../../node_modules/lit-html/lit-html.js";
-
-const dashboardTemplate = html`
-<h1>Welcome to dashbaord</h1>
-`
-
-export const dashboardView = (ctx) => {
-    return dashboardTemplate
-}
