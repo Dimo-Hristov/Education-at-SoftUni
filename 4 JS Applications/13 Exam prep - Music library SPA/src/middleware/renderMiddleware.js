c72e60a7b49ce5869420f5c828e385b40cea109d
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-03-22 14:01:28 +0100

    navigation view done

---
+++ 4 JS Applications/13 Exam prep - Music library SPA/src/middleware/renderMiddleware.js
@@ -0,0 +1,25 @@
+import { render } from '../../node_modules/lit-html/lit-html.js';
+import { navigationView } from '../views/navigationView.js';
+import page from '../../node_modules/page/page.mjs'
+
+const headerElement = document.querySelector('.header-content');
+const contentElement = document.querySelector('.main-content')
+
+const ctxRender = (templateResult) => {
+    render(templateResult, contentElement)
+}
+
+export const renderNavBar = (ctx, next) => {
+    render(navigationView(ctx), headerElement);
+    next();
+}
+
+export const renderContentMiddleware = (ctx, next) => {
+    ctx.render = ctxRender;
+    next();
+}
+
+export const redirectMiddleware = (ctx, next) => {
+    ctx.redirect = (path) => page.redirect(path)
+    next();
+}
