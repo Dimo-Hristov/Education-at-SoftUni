763bb3fa2dd117e2a9d611a75dabf6a2d2584f1e
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-03-23 11:25:40 +0100

    navbar done

---
+++ 4 JS Applications/14 Exam prep- Sole mates SPA/src/views/navigationView.js
@@ -0,0 +1,31 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+
+const userLinks = html`
+<div class="user">
+    <a href="/add">Add Pair</a>
+    <a href="/logout">Logout</a>
+</div>
+`;
+
+const guestLinks = html`
+<div class="guest">
+    <a href="/login">Login</a>
+    <a href="/register">Register</a>
+</div>
+`;
+
+const navtemplate = (user) => html`
+<a id="logo" href="/"><img id="logo-img" src="./images/logo.png" alt="" /></a>
+
+<nav>
+    <div>
+        <a href="/dashboard">Dashboard</a>
+        <a href="/search">Search</a>
+    </div>
+    ${user ? userLinks : guestLinks}
+</nav>
+`;
+
+export const navigationView = (ctx) => {
+    return navtemplate(ctx.user);
+};
