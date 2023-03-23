b59d06ff0f218a598adef3c552ef931e1c559402
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-03-23 21:52:03 +0100

    dashboard done

--- 4 JS Applications/14 Exam prep- Sole mates SPA/src/app.js
+++ 4 JS Applications/14 Exam prep- Sole mates SPA/src/app.js
@@ -2,6 +2,7 @@ import page from '../node_modules/page/page.mjs'
 import { logoutUser } from './api/users.js';
 import { authMiddleware } from './middleware/authMiddleware.js';
 import { redirectMiddleware, renderContentMiddleware, renderNavBar } from './middleware/renderMiddleware.js';
+import { dashboardView } from './views/dashboardView.js';
 import { homeView } from './views/homeView.js';
 import { loginView } from './views/loginView.js';
 import { registerView } from './views/registerView.js';
@@ -16,7 +17,7 @@ page('/', homeView)
 page('/login', loginView);
 page('/register', registerView);
 page('/logout', logoutUser);
-// page('/dashboard', dashboardView);
+page('/dashboard', dashboardView);
 // page('/create', createView);
 // page('/details/:id', detailsView);
 // page('/edit/:id', editView);
---
+++ 4 JS Applications/14 Exam prep- Sole mates SPA/src/views/dashboardView.js
@@ -0,0 +1,36 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import { getAllArticles } from "../api/data.js";
+
+const articleTemplate = (article) => html`
+<li class="card">
+    <img src="${article.imageUrl}" alt="travis" />
+    <p>
+        <strong>Brand: </strong><span class="brand">${article.brand}</span>
+    </p>
+    <p>
+        <strong>Model: </strong><span class="model">${article.model}</span>
+    </p>
+    <p><strong>Value:</strong><span class="value">${article.value}</span>$</p>
+    <a class="details-btn" href="/details/${article._id}">Details</a>
+</li>
+    `
+
+const dashboardTemplate = (articles) => html`
+    <section id="dashboard">
+        <h2>Collectibles</h2>
+        ${articles.length == 0
+        ? html` <h2>There are no items added yet.</h2>`
+        : html`
+        <ul class="card-wrapper">
+            ${articles.map(x => html`${articleTemplate(x)}`)}
+        </ul>
+        `}
+    </section>
+`;
+
+export const dashboardView = (ctx) => {
+    getAllArticles()
+        .then(articles => {
+            ctx.render(dashboardTemplate(articles))
+        })
+}
