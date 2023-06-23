69a39c422d27d069f2fd4fda431981d3525d2e68
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-06-23 12:29:41 +0300

    make myProp in list and set timeout in constructor

--- 6 Angular/Angular training app/src/app/list/list.component.ts
+++ 6 Angular/Angular training app/src/app/list/list.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent {
-
+  myProp = 'Hello World';
+  constructor() {
+    setTimeout(() => {
+      this.myProp = 'Hello other World!';
+    }, 1000);
+  }
 }
