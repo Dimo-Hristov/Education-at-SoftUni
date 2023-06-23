216fed3a2a457b0cedbf621edc883d6ed7a26971
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-06-23 10:51:34 +0300

    implement toogle image function

--- 6 Angular/Angular training app/training-app/src/app/article/article.component.ts
+++ 6 Angular/Angular training app/training-app/src/app/article/article.component.ts
@@ -9,10 +9,21 @@ export class ArticleComponent {
   private symbols: number = 250;
   @Input() article: Article;
   @Input() artocleeDesc: string;
-  descToShow: string;
-  articleDescLen: number;
+  descToShow!: string;
+  articleDescLen!: number;
   showReadMoreBtn: boolean = true;
   showHideBtn: boolean = false;
   iamgeIsShown: boolean = false;
   imageButtonTitle: string = 'Show Image';
+
+  constructor() {
+    this.articleDescLen = 0;
+    this.descToShow = '';
+  }
+
+  toggleImage(): void {
+    this.iamgeIsShown = !this.iamgeIsShown;
+    this.imageButtonTitle =
+      this.imageButtonTitle === 'Show Image' ? 'Hide Image' : 'Show Image';
+  }
 }
