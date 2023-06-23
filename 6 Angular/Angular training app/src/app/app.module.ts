907969aeebaa309ad18a66005e13b8d5505c4ca1
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-06-23 13:29:55 +0300

    make list-item component and move the list-items there

--- 6 Angular/Angular training app/src/app/app.module.ts
+++ 6 Angular/Angular training app/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
-  declarations: [AppComponent, ListComponent],
+  declarations: [AppComponent, ListComponent, ListItemComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ 6 Angular/Angular training app/src/app/list-item/list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-item',
+  templateUrl: './list-item.component.html',
+  styleUrls: ['./list-item.component.css'],
+})
+export class ListItemComponent {
+  user!: { firstName: string; lastName: string };
+  constructor() {}
+}
