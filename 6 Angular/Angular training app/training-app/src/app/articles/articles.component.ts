89ec493882b3ebb06a936566527f62648cde2c49
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-06-23 11:12:34 +0300

    Implement Logic Inside the Component

--- 6 Angular/Angular training app/training-app/src/app/articles/articles.component.ts
+++ 6 Angular/Angular training app/training-app/src/app/articles/articles.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Article } from '../models/article.model';
+import { ArticleData } from '../data/data';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.css']
+  styleUrls: ['./articles.component.css'],
 })
 export class ArticlesComponent {
+  articles: Article[];
+  constructor() {}
 
+  ngOnInit() {
+    this.articles = new ArticleData().getData();
+  }
 }
