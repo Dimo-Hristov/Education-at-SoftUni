b56a6cd5516632b7b83f3285b785cc918c7ee400
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-06-23 12:17:20 +0300

    add same styles to list comp and app comp to see that they are not mixed

--- 6 Angular/Angular training app/src/app/app.module.ts
+++ 6 Angular/Angular training app/src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ArticleComponent } from './article/article.component';
-import { ArticlesComponent } from './article/articles/articles.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
-  declarations: [AppComponent, ArticleComponent, ArticlesComponent],
+  declarations: [AppComponent, ListComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
