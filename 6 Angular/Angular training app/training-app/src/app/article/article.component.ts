9c0aa79a6761aec7480a53e52d7b2942cb23225b
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-06-23 10:46:20 +0300

    declare components in articlecomponents

--- 6 Angular/Angular training app/training-app/src/app/article/article.component.ts
+++ 6 Angular/Angular training app/training-app/src/app/article/article.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent {
-
+  private symbols: number = 250;
+  @Input() article: Article;
+  @Input() artocleeDesc: string;
+  descToShow: string;
+  articleDescLen: number;
+  showReadMoreBtn: boolean = true;
+  showHideBtn: boolean = false;
+  iamgeIsShown: boolean = false;
+  imageButtonTitle: string = 'Show Image';
 }
