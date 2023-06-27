21496f616ae1145118ba6864089c49b0140d90ec
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-06-27 21:54:45 +0300

    implement readMore function

--- 6 Angular/02 Components/articles-app/src/app/article/article.component.ts
+++ 6 Angular/02 Components/articles-app/src/app/article/article.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Article } from '../article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent {
+  private symbols: number = 250;
+  @Input() article!: Article;
+  @Input() articleDesc!: string;
+  descToShow: string;
+  articleDescLen: number;
+  showReadMoreBtn: boolean = true;
+  showHideBtn: boolean = false;
+  imageIsShown: boolean = false;
+  imageButtonTitle: string = 'Show Image';
 
+  constructor() {
+    this.articleDescLen = 0;
+    this.descToShow = '';
+  }
+
+  readMore(): void {
+    this.articleDescLen == this.symbols;
+    if (this.articleDescLen >= this.articleDesc.length) {
+      this.showHideBtn = true;
+      this.showReadMoreBtn = false;
+    } else {
+      this.descToShow = this.articleDesc.substring(0, this.articleDescLen);
+    }
+  }
 }
