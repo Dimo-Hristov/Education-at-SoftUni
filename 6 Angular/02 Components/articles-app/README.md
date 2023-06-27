5d8121d603d1244d7fae385028fa64519d51a05b
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-06-27 21:00:46 +0300

    change the title and add  css

--- 6 Angular/02 Components/articles-app/README.md
+++ 6 Angular/02 Components/articles-app/README.md
@@ -1,4 +1,4 @@
-# TrainingApp
+# ArticlesApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.1.
 
--- 6 Angular/02 Components/articles-app/src/app/app.component.ts
+++ 6 Angular/02 Components/articles-app/src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Article Site';
-
-  changeTitleHandler(newTitle: string) {
-    this.title = newTitle;
-  }
+  title = 'article Site';
 }
