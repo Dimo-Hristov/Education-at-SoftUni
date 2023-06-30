36af85a8e21a90660a8b95169f5f347841115fca
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-06-30 19:18:04 +0300

    make changeDetection to onPush

--- 6 Angular/03 DI, Intro to RxJS, Services/demo-app/src/app/users-list/users-list.component.ts
+++ 6 Angular/03 DI, Intro to RxJS, Services/demo-app/src/app/users-list/users-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { User } from '../types/user';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UsersListComponent {
   @Input() users: User[] = [];
