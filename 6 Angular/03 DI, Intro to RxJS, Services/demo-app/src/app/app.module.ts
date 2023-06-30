16428df7eadead45a7d4c8152f7509046db083fe
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-06-30 18:41:32 +0300

    start from the begining

--- 6 Angular/03 DI, Intro to RxJS, Services/demo-app/src/app/app.module.ts
+++ 6 Angular/03 DI, Intro to RxJS, Services/demo-app/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsersListComponent } from './users-list/users-list.component';
-import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UsersListComponent,
-    UserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
