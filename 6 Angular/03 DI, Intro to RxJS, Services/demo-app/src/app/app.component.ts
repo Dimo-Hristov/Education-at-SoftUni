cc7d0b2b6ba608c2bc46725c5b41bcbe52656b35
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-06-30 18:07:14 +0300

    pass the user data to child and make User interface

--- 6 Angular/03 DI, Intro to RxJS, Services/demo-app/src/app/app.component.ts
+++ 6 Angular/03 DI, Intro to RxJS, Services/demo-app/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { User } from './types/user-type';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'demo-app';
+
+  users: User[] = [
+    { name: 'Pesho', age: 51, email: 'asdasd@abv.bg' },
+    { name: 'Gosho', age: 41, email: 'asdasd@abv.bg' },
+    { name: 'Ceko', age: 31, email: 'asdasd@abv.bg' },
+    { name: 'Ivan', age: 21, email: 'asdasd@abv.bg' },
+  ];
 }
--- 6 Angular/03 DI, Intro to RxJS, Services/demo-app/src/app/users-list/users-list.component.ts
+++ 6 Angular/03 DI, Intro to RxJS, Services/demo-app/src/app/users-list/users-list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from '../types/user-type';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.css']
+  styleUrls: ['./users-list.component.css'],
 })
 export class UsersListComponent {
-
+  @Input() users: User[] = [];
 }
