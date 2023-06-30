ff63f6a0a45c6b3a783590c3fba7a7c19928a462
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-06-30 19:15:14 +0300

    make inputs and button to add new user and implement  func

--- 6 Angular/03 DI, Intro to RxJS, Services/demo-app/src/app/app.component.ts
+++ 6 Angular/03 DI, Intro to RxJS, Services/demo-app/src/app/app.component.ts
@@ -15,4 +15,21 @@ export class AppComponent {
     { name: 'Ceko', age: 31, email: 'asdasd@abv.bg' },
     { name: 'Ivan', age: 21, email: 'asdasd@abv.bg' },
   ];
+
+  addUser(
+    inputNameEl: HTMLInputElement,
+    inputAgeEl: HTMLInputElement,
+    inputEmailEl: HTMLInputElement
+  ) {
+    const user = {
+      name: inputNameEl.value,
+      age: Number(inputAgeEl.value),
+      email: inputEmailEl.value,
+    };
+    this.users.push(user);
+
+    inputNameEl.value = '';
+    inputAgeEl.value = '';
+    inputEmailEl.value = '';
+  }
 }
