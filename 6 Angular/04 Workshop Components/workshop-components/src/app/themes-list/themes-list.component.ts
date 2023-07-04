5331f6ec78054e900a767df491ea7a5ae4aae2dc
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-04 12:50:57 +0300

    use ngFor and complet the reder template for themes

--- 6 Angular/04 Workshop Components/workshop-components/src/app/themes-list/themes-list.component.ts
+++ 6 Angular/04 Workshop Components/workshop-components/src/app/themes-list/themes-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Theme } from '../types/theme';
 
 @Component({
   selector: 'app-themes-list',
@@ -7,11 +8,15 @@ import { ApiService } from '../api.service';
   styleUrls: ['./themes-list.component.css'],
 })
 export class ThemesListComponent implements OnInit {
+  themesList: Theme[] = [];
+
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.apiService.getThemes().subscribe((themes) => {
-      console.log(themes);
+      console.log(themes[0]);
+
+      this.themesList = themes;
     });
   }
 }
