43aece1324f93fd0ccecd6bd0081c31d2bcdf905
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-04 09:16:47 +0300

    implement getPost and get Themes functions in api

--- 6 Angular/04 Workshop Components/workshop-components/src/app/api.service.ts
+++ 6 Angular/04 Workshop Components/workshop-components/src/app/api.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  constructor(private http: HttpClientModule) {}
+  constructor(private http: HttpClient) {}
 
-  getPosts() {}
-  getTeams() {}
+  getPosts() {
+    const { appUrl } = environment;
+    return this.http.get(`${appUrl}/themes`);
+  }
+  getThemes(limit?: number) {
+    const { appUrl } = environment;
+    const limitFilter = limit ? `?limit=${limit}` : '';
+
+    return this.http.get(`${appUrl}/posts${limitFilter}`);
+  }
 }
