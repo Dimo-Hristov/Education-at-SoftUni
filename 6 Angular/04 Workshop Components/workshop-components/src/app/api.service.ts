8b6cb34848ae410482a175439520329ea6683957
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-04 09:22:56 +0300

    get data from server in posts and themes list done

--- 6 Angular/04 Workshop Components/workshop-components/src/app/api.service.ts
+++ 6 Angular/04 Workshop Components/workshop-components/src/app/api.service.ts
@@ -8,14 +8,14 @@ import { environment } from 'src/environments/environment.development';
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  getPosts() {
-    const { appUrl } = environment;
-    return this.http.get(`${appUrl}/themes`);
-  }
-  getThemes(limit?: number) {
+  getPosts(limit?: number) {
     const { appUrl } = environment;
     const limitFilter = limit ? `?limit=${limit}` : '';
 
     return this.http.get(`${appUrl}/posts${limitFilter}`);
+  }
+  getThemes() {
+    const { appUrl } = environment;
+    return this.http.get(`${appUrl}/themes`);
   }
 }
--- 6 Angular/04 Workshop Components/workshop-components/src/app/posts-list/posts-list.component.ts
+++ 6 Angular/04 Workshop Components/workshop-components/src/app/posts-list/posts-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
-  styleUrls: ['./posts-list.component.css']
+  styleUrls: ['./posts-list.component.css'],
 })
-export class PostsListComponent {
+export class PostsListComponent implements OnInit {
+  constructor(private apiService: ApiService) {}
 
+  ngOnInit(): void {
+    this.apiService.getPosts(5).subscribe((posts) => {
+      console.log(posts);
+    });
+  }
 }
