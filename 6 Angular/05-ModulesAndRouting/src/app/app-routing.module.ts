2cb04d05660c84f0abc597b2c7b792503acbecba
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-06 11:01:58 +0300

    move users-list to mudle routing

--- 6 Angular/05-ModulesAndRouting/src/app/app-routing.module.ts
+++ 6 Angular/05-ModulesAndRouting/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserListComponent } from './user/user-list/user-list.component';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: UserListComponent },
-  { path: 'user-list', component: UserListComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'user/list' },
   { path: 'todo-list', component: TodoListComponent },
 ];
 
--- 6 Angular/05-ModulesAndRouting/src/app/user/user.module.ts
+++ 6 Angular/05-ModulesAndRouting/src/app/user/user.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
+import { RouterModule } from '@angular/router';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
-  declarations: [UserListComponent],
-  imports: [CommonModule],
+  declarations: [UserListComponent, UserDetailComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: 'user/list',
+        component: UserListComponent,
+      },
+      {
+        path: 'user/list/:id',
+        component: UserDetailComponent,
+      },
+    ]),
+  ],
   exports: [UserListComponent],
 })
 export class UserModule {}
