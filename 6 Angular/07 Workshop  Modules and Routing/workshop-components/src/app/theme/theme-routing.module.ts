609fd39c3437bba5aca1f1cae46fb3e9c7b79c05
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-11 16:52:24 +0300

    add routing to currentTheme component

--- 6 Angular/07 Workshop  Modules and Routing/workshop-components/src/app/theme/theme-routing.module.ts
+++ 6 Angular/07 Workshop  Modules and Routing/workshop-components/src/app/theme/theme-routing.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from '../main/main.component';
 import { NewThemeComponent } from './new-theme/new-theme.component';
+import { CurrentThemeComponent } from './current-theme/current-theme.component';
 
 const routes: Routes = [
   {
     path: 'themes',
-    component: MainComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: MainComponent,
+      },
+      {
+        path: ':themeId',
+        component: CurrentThemeComponent,
+      },
+    ],
   },
   {
     path: 'new-theme',
