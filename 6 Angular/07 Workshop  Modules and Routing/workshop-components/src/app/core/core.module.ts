aa2c9f28ca716ce59c2bc21df17afb859030bf09
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-11 16:27:02 +0300

    attach routerLink to header buttons

--- 6 Angular/07 Workshop  Modules and Routing/workshop-components/src/app/core/core.module.ts
+++ 6 Angular/07 Workshop  Modules and Routing/workshop-components/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   //Note: tells wich components to be exorted from this module
   exports: [HeaderComponent, FooterComponent],
 })
