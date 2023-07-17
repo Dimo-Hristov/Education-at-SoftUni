66b7535aa540997fed9e9375356c2fa91eea8eec
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-17 13:43:45 +0300

    add app-email validator with regExp

---
+++ 6 Angular/08 Forms/workshop-components/src/app/shared/validators/app-email-validator.ts
@@ -0,0 +1,11 @@
+import { ValidatorFn } from '@angular/forms';
+
+export function appEmailValidator(domains: string[]): ValidatorFn {
+  const domainStrings = domains.join('|');
+  const regExp = new RegExp(`/[^@]{6,}@gmail\.(${domainStrings})$/gm`);
+  return (control) => {
+    return control.value === '' || regExp.test(control.value)
+      ? null
+      : { appEmailValidator: true };
+  };
+}
--- 6 Angular/08 Forms/workshop-components/src/app/shared/validators/app-email.directive.ts
+++ 6 Angular/08 Forms/workshop-components/src/app/shared/validators/app-email.directive.ts
@@ -1,10 +1,13 @@
 import { Directive } from '@angular/core';
+import { AbstractControl, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
-  selector: '[appAppEmail]'
+  selector: '[appEmail]',
 })
-export class AppEmailDirective {
+export class AppEmailDirective implements Validator {
+  constructor() {}
 
-  constructor() { }
-
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    return null;
+  }
 }
