011ad7546680783c4c46e69d794bf193f7837a0b
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-09-14 09:47:59 +0300

    test writeStram complete

---
+++ 7 JS Back-End/02 Node.js Streams and Utilities/streams/writeStream.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const writeStream = fs.createWriteStream('./output.txt', 'utf-8');
+
+
+writeStream.write('Chunk 1');
+writeStream.write('Chunk 2');
+writeStream.write('Chunk 3');
+writeStream.write('Chunk 4');
+writeStream.end();
