22bc9ef481db7386b1cdad15aeab08354646072c
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-09-14 09:44:55 +0300

    testing on end

--- 7 JS Back-End/02 Node.js Streams and Utilities/streams/readableStream.js
+++ 7 JS Back-End/02 Node.js Streams and Utilities/streams/readableStream.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream('./input.txt', 'utf-8');
+const readStream = fs.createReadStream('./input.txt');
 
 readStream.on('data', (chunk) => {
     console.log('Read chunk');
     console.log(chunk);
+});
+
+readStream.on('end', () => {
+    console.log('Reading data is finished');
 })
