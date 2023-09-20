b779cee8658c79ee07f023867162c0494e1a84ed
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-09-20 13:30:37 +0300

    use statoc folder

--- 7 JS Back-End/03 ExpressJS and Templating/index.js
+++ 7 JS Back-End/03 ExpressJS and Templating/index.js
@@ -1,80 +1,88 @@
-const express = require('express');
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 //Add third party middleware
-const bodyParser = express.urlencoded({ extended: false })
+const bodyParser = express.urlencoded({ extended: false });
 app.use(bodyParser);
 
 
+// static files directory middleware
+app.use(express.static('public'))
+
 // Middlewares
 app.use((req, res, next) => {
-    console.log('Middleware 1');
+    console.log("Middleware 1");
     next();
-})
-
+});
 
 app.use((req, res, next) => {
     console.log(`HTTP Request ${req.method}: ${req.path}`);
     next();
-})
-
-
+});
 
 //Express Router - Actions
 
-app.get('/', (req, res) => {
-    res.send('Hello from express');
+app.get("/", (req, res) => {
+    res.send("Hello from express");
 });
 
-app.get('/cats', (req, res) => {
-    res.send(`<form method="post"> 
+app.get("/cats", (req, res) => {
+    res.send(`
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post"> 
     <label for="name">Name</label>
     <input type="text" name="name" id="name"/>
     <label for="age">Age</label>
     <input type"number" name="age" id="age"/>
     <button type="submit">Submit</button>
-    </form>`);
+    </form>
+        
+</body>
+</html>`);
 });
 
-app.get('/cats/:catId', (req, res) => {
-    const catId = Number(req.params.catId)
+app.get("/cats/:catId", (req, res) => {
+    const catId = Number(req.params.catId);
     if (!catId) {
-        res.status(404).send('Cannot find cat');
-        return
+        res.status(404).send("Cannot find cat");
+        return;
     }
     console.log(req.params);
-    res.send(`Request with parameter = ${catId} `)
+    res.send(`Request with parameter = ${catId} `);
 });
 
-app.post('/cats', (req, res) => {
+app.post("/cats", (req, res) => {
     console.log(req.body);
-    res.status(201).send('Cat hard been created')
+    res.status(201).send("Cat hard been created");
 });
 
-app.get('/download', (req, res) => {
+app.get("/download", (req, res) => {
     // download the file
     // res.download('./03 ExpressJS and Templating/manual.jpg');
-
     // if we want to attack more responses use .attachment, need to end()
     // res.attachment('./03 ExpressJS and Templating/manual.jpg');
     // res.end();
-
-
     //open the file in the browser tab
     // res.sendFile(path.resolve(__dirname, 'manual.jpg'))
-
 });
 
-app.get('/old-route', (req, res) => {
-    res.redirect('/cats');
+app.get("/old-route", (req, res) => {
+    res.redirect("/cats");
 });
-
 
 // not found request should be always in the end.
-app.get('/*', (req, res) => {
-    res.status(404).send('Not found!')
+app.get("/*", (req, res) => {
+    res.status(404).send("Not found!");
 });
 
-app.listen(5000, () => console.log('Server is listening on port 5000...'));
+app.listen(5000, () => console.log("Server is listening on port 5000..."));
