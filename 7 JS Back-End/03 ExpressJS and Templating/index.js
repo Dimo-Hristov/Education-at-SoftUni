d29459e871cb3e6d68eb9b7dbb87fb796a6f9873
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-09-20 14:39:32 +0300

    add  layout

--- 7 JS Back-End/03 ExpressJS and Templating/index.js
+++ 7 JS Back-End/03 ExpressJS and Templating/index.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 
 app.get("/", (req, res) => {
     // res.send("Hello from express");
-    res.render('home', { layout: false });
+    res.render('home');
 });
 
 app.get("/cats", (req, res) => {
