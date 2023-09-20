2f43b03e5d7f1f4647f003757ae8652e22e3c413
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-09-20 10:44:06 +0300

    create second path

--- 7 JS Back-End/03 ExpressJS and Templating/index.js
+++ 7 JS Back-End/03 ExpressJS and Templating/index.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) => {
     res.send('Hello from express');
 });
 
+app.get('/cats', (req, res) => {
+    res.send('This page contains cats')
+})
+
 app.listen(5000, () => console.log('Server is listening on port 5000...'));
