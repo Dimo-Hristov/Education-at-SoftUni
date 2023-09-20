310afe49c91d4f7a4c123700c037c20ba6495124
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-09-20 10:54:35 +0300

    request with parameter done

--- 7 JS Back-End/03 ExpressJS and Templating/index.js
+++ 7 JS Back-End/03 ExpressJS and Templating/index.js
@@ -8,14 +8,20 @@ app.get('/', (req, res) => {
 
 app.get('/cats', (req, res) => {
     res.send('This page contains cats')
-})
+});
+
+app.get('/cats/:catId', (req, res) => {
+    res.send('Request with parameter')
+});
 
 app.post('/cats', (req, res) => {
     res.status(201).send('Cat hard been created')
-})
+});
 
+
+// not found request should be always in the end.
 app.get('/*', (req, res) => {
     res.status(404).send('Not found!')
-})
+});
 
 app.listen(5000, () => console.log('Server is listening on port 5000...'));
