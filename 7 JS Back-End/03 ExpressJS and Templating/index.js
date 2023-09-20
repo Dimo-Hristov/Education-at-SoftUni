5a6eef3e0fdd10ae4af4ee1aa1c88ca214050ac4
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-09-20 10:48:16 +0300

    Add post request to /cats, done

--- 7 JS Back-End/03 ExpressJS and Templating/index.js
+++ 7 JS Back-End/03 ExpressJS and Templating/index.js
@@ -10,4 +10,8 @@ app.get('/cats', (req, res) => {
     res.send('This page contains cats')
 })
 
+app.post('/cats', (req, res) => {
+    res.status(201).send('Cat hard been created')
+})
+
 app.listen(5000, () => console.log('Server is listening on port 5000...'));
