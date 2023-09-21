54e3ba756cc5e46ddd2e717fa0c049bc13a9ea6d
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-09-21 10:23:16 +0300

    setup view engine done

--- 7 JS Back-End/04 Workshop ExpressJS and Templating/config/express.js
+++ 7 JS Back-End/04 Workshop ExpressJS and Templating/config/express.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
+const path = require('path')
 
 module.exports = (app) => {
-    
+
     //TODO: Setup the view engine
+    app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+    app.set('view engine', 'hbs');
+    app.set('views', path.join(__dirname, 'views'));
 
     //TODO: Setup the body parser
 
