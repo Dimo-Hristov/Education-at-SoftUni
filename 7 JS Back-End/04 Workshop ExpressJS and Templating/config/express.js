7bba2f3ce7d546deba8ed1b83fadf66199def29b
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-09-21 11:16:03 +0300

    add paths

--- 7 JS Back-End/04 Workshop ExpressJS and Templating/config/express.js
+++ 7 JS Back-End/04 Workshop ExpressJS and Templating/config/express.js
@@ -8,13 +8,14 @@ module.exports = (app) => {
     //TODO: Setup the view engine
     app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
     app.set('view engine', 'hbs');
-    app.set('views', path.join(__dirname, 'views'));
+    app.set('views', path.join(__dirname, '..', 'views'));
+
 
     //TODO: Setup the body parser
     const bodyParser = express.urlencoded({ extended: false })
     app.use(bodyParser)
 
     //TODO: Setup the static files
-    app.use(express.static('./04 Workshop ExpressJS and Templating/static'));
+    app.use(express.static('./04 Workshop ExpressJS and Templating/static'))
 
 };
--- 7 JS Back-End/04 Workshop ExpressJS and Templating/config/routes.js
+++ 7 JS Back-End/04 Workshop ExpressJS and Templating/config/routes.js
@@ -1,5 +1,24 @@
 // TODO: Require Controllers...
 
 module.exports = (app) => {
-    // TODO...
+    app.get('/', (req, res) => {
+        res.render('index', { layout: false })
+    })
+
+    app.get('/about', (req, res) => {
+        res.render('about', { layout: false })
+    });
+
+    app.get('/create', (req, res) => {
+        res.render('create', { layout: false })
+    })
+
+    app.get('/details/:id', (req, res) => {
+        console.log(req.params);
+        res.render('details', { layout: false })
+    })
+
+    app.get('*', (req, res) => {
+        res.render('404', { layout: false })
+    })
 };
