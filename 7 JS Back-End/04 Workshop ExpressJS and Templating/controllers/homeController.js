233f0935ec0fadc65b155b058308de8b23f399cf
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-09-25 10:13:35 +0300

    move about route to homecontroller

--- 7 JS Back-End/04 Workshop ExpressJS and Templating/controllers/homeController.js
+++ 7 JS Back-End/04 Workshop ExpressJS and Templating/controllers/homeController.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index');
 });
+
+router.get('/about', (req, res) => {
+    res.render('about')
+})
 
 
 module.exports = router
