eb673b1a3726d892135264b66e02fed68fd7062b
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-09-25 12:29:42 +0300

    make create form to send POST req to /cubes/create

--- 7 JS Back-End/04 Workshop ExpressJS and Templating/controllers/cubeController.js
+++ 7 JS Back-End/04 Workshop ExpressJS and Templating/controllers/cubeController.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 
 router.get('/create', (req, res) => {
     res.render('create')
-})
+});
+
+router.post('/create', (req, res) => {
+    console.log(req.body);
+});
 
 module.exports = router
