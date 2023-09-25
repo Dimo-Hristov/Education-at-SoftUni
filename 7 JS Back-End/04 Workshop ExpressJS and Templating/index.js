562e4057ed4d40524f67a4661f2908a8a6136784
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-09-25 10:08:39 +0300

    make home controllet with module routing

--- 7 JS Back-End/04 Workshop ExpressJS and Templating/index.js
+++ 7 JS Back-End/04 Workshop ExpressJS and Templating/index.js
@@ -1,9 +1,12 @@
 const env = process.env.NODE_ENV || 'development';
+const homeController = require('./controllers/homeController')
 
 const config = require('./config/config')[env];
 const app = require('express')();
 
 require('./config/express')(app);
 require('./config/routes')(app);
+
+app.use(homeController)
 
 app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
