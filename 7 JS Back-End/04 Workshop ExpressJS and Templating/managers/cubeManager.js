4566947e8fd757a21c6272f4ff97c299871a3d4e
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-09-26 10:04:55 +0300

    implement search logic in get all function

--- 7 JS Back-End/04 Workshop ExpressJS and Templating/managers/cubeManager.js
+++ 7 JS Back-End/04 Workshop ExpressJS and Templating/managers/cubeManager.js
@@ -1,7 +1,23 @@
 const uniqid = require('uniqid')
 const cubes = [];
 
-exports.getAll = () => cubes.slice();
+exports.getAll = (search, from, to) => {
+    let result = cubes.slice();
+
+    if (search) {
+        result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()))
+    }
+
+    if (from) {
+        result = result.filter(cube => cube.difficultyLevel >= from);
+    }
+
+    if (to) {
+        result = result.filter(cube => cube.difficultyLevel <= to);
+    }
+
+    return result
+}
 
 exports.find = (cubeId) => cubes.find(cube => cube.id == cubeId);
 
