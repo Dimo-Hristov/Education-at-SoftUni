f957ad9ddb373882e2b107784c010d94c9e5b13f
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-09-26 10:25:45 +0300

    add routes.js and move all route settings from index to it

--- 7 JS Back-End/04 Workshop ExpressJS and Templating/index.js
+++ 7 JS Back-End/04 Workshop ExpressJS and Templating/index.js
@@ -1,16 +1,11 @@
 const env = process.env.NODE_ENV || 'development';
-const homeController = require('./controllers/homeController');
-const cubeController = require('./controllers/cubeController')
 
 const config = require('./config/config')[env];
 const app = require('express')();
+const routes = require('./routes')
 
 require('./config/express')(app);
 
-app.use(homeController);
-app.use('/cubes', cubeController);
-app.get('*', (req, res) => {
-    res.redirect('/404')
-})
+app.use(routes)
 
 app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
