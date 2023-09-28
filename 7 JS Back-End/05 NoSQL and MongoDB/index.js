c3d8d9dc0536b7e3d711b40d420089bd14fa7d9e
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-09-28 11:18:44 +0300

    add come cats to the db done

--- 7 JS Back-End/05 NoSQL and MongoDB/index.js
+++ 7 JS Back-End/05 NoSQL and MongoDB/index.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 
+const Cat = require('./models/Cat')
+
 async function connectDb() {
     await mongoose.connect('mongodb://localhost:27017/test');
     console.log('Database connected....');
 
 
+    const newCat = new Cat({
+        name: 'Gosho',
+        age: 15,
+        breed: 'kuche'
+    });
 
+    await newCat.save();
 
 }
 connectDb();
