bcdf89dd30f793a76dcf40785495bc1c797e5b49
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-06 11:51:44 +0300

    add post action for create accesory

--- 7 JS Back-End/06 Workshop MongoDB and Mongoose/controllers/accesoryController.js
+++ 7 JS Back-End/06 Workshop MongoDB and Mongoose/controllers/accesoryController.js
@@ -4,4 +4,14 @@ router.get('/create', (req, res) => {
     res.render('accesory/create');
 });
 
+router.post('/create', (req, res) => {
+    const body = req.body;
+
+    console.log(body);
+
+    // TODO: add accesory data to database
+
+    res.redirect('/')
+})
+
 module.exports = router;
