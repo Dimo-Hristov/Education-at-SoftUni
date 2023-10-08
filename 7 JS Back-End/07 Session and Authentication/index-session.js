ba96074174909bf07c05ac93ecbf9b7671d70fb6
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-08 22:28:54 +0300

    save session demos's and make new index

--- 7 JS Back-End/07 Session and Authentication/index-session.js
+++ 7 JS Back-End/07 Session and Authentication/index-session.js
@@ -6,7 +6,12 @@ const { v4: uuid } = require('uuid');
 const app = express();
 
 app.use(cookieParser());
-
+app.use(expressSession({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: true,
+    coookie: { secure: false }
+}))
 
 const session = {};
 
@@ -18,9 +23,10 @@ app.get('/', (req, res) => {
 
     if (userId) {
         id = userId;
-
+        console.log(req.session.secret);
     } else {
         id = uuid();
+        req.session.secret = `${id} some secret`;
         res.cookie('userId', id, { httpOnly: true });
     }
 
