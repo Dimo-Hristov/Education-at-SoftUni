b036f93e3488a37358a85b9509e04069d9a91c75
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-08 22:50:06 +0300

    make salt and hash the password, send it to the browser

--- 7 JS Back-End/07 Session and Authentication/index.js
+++ 7 JS Back-End/07 Session and Authentication/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const expressSession = require('express-session');
 const { v4: uuid } = require('uuid');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
-
-const session = {};
 
 app.get('/', (req, res) => {
     let id = undefined;
@@ -26,5 +25,27 @@ app.get('/', (req, res) => {
 
     res.send(`Hello user - ${id}`);
 });
+
+app.get('/login', (req, res) => {
+    res.send(`
+        <form method="POST">
+    <label for="username">Username</label>
+    <input type="text" name="username" id="username" />
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password" />
+    <input type="submit" value="Login" />
+    </form>`)
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+
+
+    res.send(hash);
+})
 
 app.listen(5000, () => console.log('Server is running'))
