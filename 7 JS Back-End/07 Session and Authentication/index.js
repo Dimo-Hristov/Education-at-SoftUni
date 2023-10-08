7e6156d27509658bd3bd9631ba892def2b5aaaf9
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-08 16:55:09 +0300

    create secret session

--- 7 JS Back-End/07 Session and Authentication/index.js
+++ 7 JS Back-End/07 Session and Authentication/index.js
@@ -4,17 +4,24 @@ const { v4: uuid } = require('uuid');
 
 const app = express();
 
-app.use(cookieParser())
+app.use(cookieParser());
+
+const session = {};
 
 app.get('/', (req, res) => {
     let id = undefined;
 
     const userId = req.cookies['userId'];
 
+
     if (userId) {
         id = userId;
+        console.log('User secret: ', session[userId].secret);
     } else {
         id = uuid();
+        session[id] = {
+            secret: 'My secret',
+        };
         res.cookie('userId', id);
     }
 
