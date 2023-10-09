72525e9265302c2d02c6d61d2a449a3e2cc0496d
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-09 22:07:48 +0300

    create userSchema

--- 7 JS Back-End/08 Workshop Session and Authentication/controllers/userController.js
+++ 7 JS Back-End/08 Workshop Session and Authentication/controllers/userController.js
@@ -2,6 +2,14 @@ const router = require('express').Router();
 
 router.get('/register', (req, res) => {
     res.render('user/register')
+});
+
+router.post('/register', (req, res) => {
+    const { username, password, repeatPassword } = req.body;
+
+    console.log(username, password, repeatPassword);
+
+    res.redirect('/user/login');
 })
 
 module.exports = router;
