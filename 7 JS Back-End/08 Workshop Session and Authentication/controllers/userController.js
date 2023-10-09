6d59625bd943f49269ab44c2fc58543d174ca551
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-09 22:39:20 +0300

    save user with password validation in model

--- 7 JS Back-End/08 Workshop Session and Authentication/controllers/userController.js
+++ 7 JS Back-End/08 Workshop Session and Authentication/controllers/userController.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
+const userManager = require('../managers/userManager')
 
 router.get('/register', (req, res) => {
     res.render('user/register')
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const { username, password, repeatPassword } = req.body;
 
-    console.log(username, password, repeatPassword);
+    await userManager.register({ username, password, repeatPassword });
 
     res.redirect('/user/login');
 })
