b791b8d07481762b49025cccf40a2117ba2373fa
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-10 11:08:38 +0300

    setup cookieParser

--- 7 JS Back-End/08 Workshop Session and Authentication/config/express.js
+++ 7 JS Back-End/08 Workshop Session and Authentication/config/express.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
-const path = require('path')
+const path = require('path');
+const cookieParser = require('cookie-parser');
 
 module.exports = (app) => {
 
@@ -13,9 +14,12 @@ module.exports = (app) => {
 
     //TODO: Setup the body parser
     const bodyParser = express.urlencoded({ extended: false })
-    app.use(bodyParser)
+    app.use(bodyParser);
+
+    // TODO: Setup cookier-parser
+    app.use(cookieParser());
 
     //TODO: Setup the static files
-    app.use(express.static('./04 Workshop ExpressJS and Templating/static'))
+    app.use(express.static('./04 Workshop ExpressJS and Templating/static'));
 
 };
