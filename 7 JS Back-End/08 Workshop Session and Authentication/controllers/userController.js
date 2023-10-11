10825db5580cb665e48978bd20f9698de2079e61
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-11 13:46:21 +0300

    logout from account

--- 7 JS Back-End/08 Workshop Session and Authentication/controllers/userController.js
+++ 7 JS Back-End/08 Workshop Session and Authentication/controllers/userController.js
@@ -28,6 +28,11 @@ router.post('/login', async (req, res) => {
     res.cookie('auth', token, { httpOnly: true });
 
     res.redirect('/');
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('auth');
+    res.redirect('/')
 })
 
 module.exports = router;
