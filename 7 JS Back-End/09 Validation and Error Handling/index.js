cfe3da3ecb1a03482f22f82f13bc177954e23459
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-12 11:03:20 +0300

    validation for strong password using library

--- 7 JS Back-End/09 Validation and Error Handling/index.js
+++ 7 JS Back-End/09 Validation and Error Handling/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const validator = require('validator')
 
 const { isAgeValid } = require('./utils/validations');
 const { validateName } = require('./middlewares/middlewares');
@@ -15,19 +16,28 @@ app.get('/', (req, res) => {
 
   <label for="age">Age</label>
   <input type="number" name="age" id="age" />
+
+  <label for="password">Password</label>
+  <input type="password" name="password" id="password" />
+
   <input type="submit" />
 </form>`);
 });
 
 app.post('/', validateName, (req, res) => {
-    const { name, age } = req.body;
+    const { name, age, password } = req.body;
     //  three types of validation
     // in the action if is specific
     // in util if we use it a couple of times
     // in middleware if we use it a lot
+    // next step is using libraries.
 
     if (!isAgeValid(age)) {
         return res.send('invalid age!');
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        return res.send('Weak password')
     }
 
     res.send('Successfull operation');
