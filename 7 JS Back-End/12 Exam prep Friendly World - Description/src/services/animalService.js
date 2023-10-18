4cdbe6151acf432b4cda03e80ddde48734add745
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-18 13:37:48 +0300

    add donate service

--- 7 JS Back-End/12 Exam prep Friendly World - Description/src/services/animalService.js
+++ 7 JS Back-End/12 Exam prep Friendly World - Description/src/services/animalService.js
@@ -5,3 +5,8 @@ exports.create = (animalData) => Animal.create(animalData);
 exports.getAll = () => Animal.find();
 
 exports.getOne = (animalId) => Animal.findById(animalId);
+
+exports.donate = (animalId, userId) => Animal.findByIdAndUpdate(
+    animalId,
+    { $push: { 'donations': { votedUser: userId } } }
+);
