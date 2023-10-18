ccfe4920d3a82920118cff077f2ff7f5a1b67317
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-18 12:50:56 +0300

    get all animals from db and render them in dashboard  page

--- 7 JS Back-End/12 Exam prep Friendly World - Description/src/controllers/homeController.js
+++ 7 JS Back-End/12 Exam prep Friendly World - Description/src/controllers/homeController.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
+const animalService = require('../services/animalService');
+const { extractErrorMsgs } = require('../utils/errorHandler');
 
 router.get('/', (req, res) => {
     res.render('home');
 });
 
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard')
+router.get('/dashboard', async (req, res) => {
+
+    try {
+        const listOfAnimals = await animalService.getAll().lean();
+        const isEmpty = listOfAnimals.length < 1;
+
+        res.render('dashboard', { listOfAnimals, isEmpty });
+
+    } catch (error) {
+        const errorMessages = extractErrorMsgs(error);
+        res.status(404).render('dashboard', { errorMessages });
+    }
 })
 
 router.get('/404', (req, res) => {
--- 7 JS Back-End/12 Exam prep Friendly World - Description/src/services/animalService.js
+++ 7 JS Back-End/12 Exam prep Friendly World - Description/src/services/animalService.js
@@ -1,3 +1,5 @@
 const Animal = require('../models/Animal');
 
 exports.create = (animalData) => Animal.create(animalData);
+
+exports.getAll = () => Animal.find();
