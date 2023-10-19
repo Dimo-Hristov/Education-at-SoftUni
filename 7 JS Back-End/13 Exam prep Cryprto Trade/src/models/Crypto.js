dc08c853361afeed909afbbf98fa4fb9824608fe
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-19 18:08:27 +0300

    crerate Crypto model

---
+++ 7 JS Back-End/13 Exam prep Cryprto Trade/src/models/Crypto.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const cryptoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+    },
+    customers: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+
+})
