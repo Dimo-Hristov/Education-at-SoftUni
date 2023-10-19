28936783ea17f9134446b091c01a131d0df60e5c
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-19 14:13:38 +0300

    initialize nerw project and upload skeleton

---
+++ 7 JS Back-End/13 Exam prep Gaming Team/src/middlewares/authMiddleare.js
@@ -0,0 +1,33 @@
+const jwt = require('../lib/jwt');
+const { SECRET } = require('../constants')
+
+exports.auth = async (req, res, next) => {
+    const token = req.cookies['token'];
+
+    if (token) {
+        try {
+            const decodedToken = await jwt.verify(token, SECRET);
+            req.user = decodedToken;
+
+            res.locals.user = decodedToken;
+            res.locals.isAuthenticated = true;
+
+            next();
+        } catch (err) {
+            console.log(err.message);
+            res.clearCookie('token');
+            res.redirect('/')
+        }
+
+    } else {
+        next();
+    }
+};
+
+exports.isAuth = (req, res, next) => {
+    if (!req.user) {
+        return res.redirect('/users/login')
+    }
+
+    next();
+}
---
+++ 7 JS Back-End/13 Exam prep Gaming Team/src/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    firstName: { type: String, required: [true, 'First name is requried'] },
+    lastName: { type: String, required: [true, 'Last name is requried'] },
+    email: {
+        type: String,
+        required: [true, 'Email is requried'],
+        unique: true,
+    },
+    password: { type: String, required: [true, 'Password is requried'] },
+});
+
+userSchema.virtual('rePassword').set(function (value) {
+    if (value !== this.password) {
+        throw new Error('Password missmatch!');
+    }
+});
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+})
+
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
