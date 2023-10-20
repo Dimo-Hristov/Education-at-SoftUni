7f7e1633f9eac75ef5eb42b97ce92938ca32b87a
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-20 12:53:13 +0300

    update Crypto schema requirements

--- 7 JS Back-End/13 Exam prep Cryprto Trade/src/models/Crypto.js
+++ 7 JS Back-End/13 Exam prep Cryprto Trade/src/models/Crypto.js
@@ -3,23 +3,38 @@ const mongoose = require('mongoose');
 const cryptoSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        minLength: [2, 'Name should be at least 2 characters long']
     },
     image: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: function (value) {
+                return /^(https?:\/\/)/mg.test(value);
+            },
+            message: 'Image url should start with https:// or http://'
+        },
     },
     price: {
         type: Number,
-        required: true
+        required: true,
+        validate: {
+            validator: function (value) {
+                return value >= 0
+            },
+            message: 'Price should be a posotive number'
+        }
     },
     description: {
         type: String,
-        required: true
+        required: true,
+        minLength: [10, 'Description should be at least 10 characters long']
     },
     paymentMethod: {
         type: String,
         required: true,
+        enum: ['crypto-wallet', 'credit-card', 'debit-card', 'paypal'],
     },
     customers: [{
         type: mongoose.Schema.Types.ObjectId,
