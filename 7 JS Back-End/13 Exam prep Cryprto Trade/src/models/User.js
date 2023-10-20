dc6b44189352508842d3ec41bc7d0c0e2e19489b
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-20 12:45:45 +0300

    update userSchema requirements

--- 7 JS Back-End/13 Exam prep Cryprto Trade/src/models/User.js
+++ 7 JS Back-End/13 Exam prep Cryprto Trade/src/models/User.js
@@ -2,13 +2,22 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: [true, 'Username name is requried'] },
+    username: {
+        type: String,
+        required: [true, 'Username name is requried'],
+        minLength: [5, 'Username should be at least 5 characters long']
+    },
     email: {
         type: String,
         required: [true, 'Email is requried'],
         unique: true,
+        minLength: [10, 'Email should be at least 10 characters long']
     },
-    password: { type: String, required: [true, 'Password is requried'] },
+    password: {
+        type: String,
+        required: [true, 'Password is requried'],
+        minLength: [4, 'Password should be at least 4 characters long']
+    },
 });
 
 userSchema.virtual('rePassword').set(function (value) {
