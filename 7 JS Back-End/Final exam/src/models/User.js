282c32f1dcee5460c08693537f582c2f4a45f82c
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-21 17:01:16 +0300

    add user requirements

--- 7 JS Back-End/Final exam/src/models/User.js
+++ 7 JS Back-End/Final exam/src/models/User.js
@@ -2,13 +2,22 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: [true, 'Last name is requried'] },
+    username: {
+        type: String,
+        required: [true, 'Username is requried'],
+        minLength: [3, 'Username should be at lest 3 characters long'],
+    },
     email: {
         type: String,
         required: [true, 'Email is requried'],
         unique: true,
+        minLength: [10, 'Email should be at lest 10 characters long'],
     },
-    password: { type: String, required: [true, 'Password is requried'] },
+    password: {
+        type: String,
+        required: [true, 'Password is requried'],
+        minLength: [4, 'Username should be at lest 4 characters long'],
+    },
 });
 
 userSchema.virtual('rePassword').set(function (value) {
