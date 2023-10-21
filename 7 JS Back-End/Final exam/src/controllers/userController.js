2acc92f4666e3853f099b12c4872cdc0ea5372b0
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-21 13:07:23 +0300

    upload skeleton and prepare for exam

---
+++ 7 JS Back-End/Final exam/src/controllers/userController.js
@@ -0,0 +1,73 @@
+const router = require('express').Router();
+const userService = require('../services/userService');
+const { extractErrorMsgs } = require('../utils/errorHandler')
+
+router.get('/login', (req, res) => {
+    res.render('user/login');
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const token = await userService.login(email, password);
+
+        res.cookie('token', token, { httpOnly: true })
+        res.redirect('/');
+
+    } catch (error) {
+        const errorMessages = extractErrorMsgs(error);
+        res.status(404).render('user/login', { errorMessages });
+    }
+
+
+});
+
+router.get('/register', (req, res) => {
+    res.render('user/register');
+});
+
+
+router.post('/register', async (req, res) => {
+    const {
+        firstName,
+        lastName,
+        email,
+        password,
+        rePassword,
+    } = req.body;
+
+    try {
+        await userService.register(
+            {
+                firstName,
+                lastName,
+                email,
+                password,
+                rePassword,
+            })
+
+        res.redirect('/users/login');
+
+    } catch (error) {
+        const errorMessages = extractErrorMsgs(error);
+
+        // If email already exists in database replace the error message with custom message
+        if (error.code === 11000 && error.keyPattern && error.keyPattern.email) {
+            errorMessages.splice(0, 1, 'Email already exists')
+        }
+
+        res.status(404).render('user/register', { errorMessages });
+
+    }
+
+
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('token');
+    res.redirect('/')
+});
+
+
+module.exports = router;
---
+++ 7 JS Back-End/Final exam/src/services/userService.js
@@ -0,0 +1,28 @@
+const User = require("../models/User");
+const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt');
+const { SECRET } = require('../constants')
+
+exports.register = (userData) => User.create(userData);
+
+exports.login = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    //  validate user
+    if (!user) {
+        throw new Error('Invalid email or password!');
+    }
+
+    // validate password
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Invalid email or password!');
+    }
+
+    const payload = { _id: user._id, email: user.email }
+
+    const token = await jwt.sign(payload, SECRET, { expiresIn: '2d' });
+
+    return token;
+};
