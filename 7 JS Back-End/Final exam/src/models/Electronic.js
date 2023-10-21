5cd8d8fa5bb6f2ef4f577429eba960f43ee37388
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-21 17:10:22 +0300

    add eletronicSchema requirements

--- 7 JS Back-End/Final exam/src/models/Electronic.js
+++ 7 JS Back-End/Final exam/src/models/Electronic.js
@@ -3,35 +3,44 @@ const mongoose = require('mongoose');
 const electronicSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'Name is required'],
+        minLength: [10, 'Name should be at lest 10 characters long'],
     },
     type: {
         type: String,
-        required: true
+        required: [true, 'Type is required'],
+        minLength: [2, 'Type should be at lest 3 characters long'],
     },
     damages: {
         type: String,
-        required: true
+        required: [true, 'Damages is required'],
+        minLength: [10, 'Damages should be at lest 3 characters long'],
     },
     image: {
         type: String,
-        required: true
+        required: ['Image is required'],
+        validate: {
+            validator: function (value) {
+                return /^(https?:\/\/)/mg.test(value);
+            },
+            message: 'Image url should start with https:// or http://'
+        },
     },
     description: {
         type: String,
-        required: true
+        required: [true, 'Description is required'],
+        minLength: [10, 'Description should be at lest 3 characters long'],
+        maxLength: [200, 'Maximum characters allowed is 200'],
     },
     production: {
         type: Number,
-        required: true
-    },
-    image: {
-        type: String,
-        required: true
+        required: true,
+        min: [1900, 'Production should be between 1900 and 2023'],
+        max: [1900, 'Production should be between 1900 and 2023'],
     },
     exploitation: {
         type: Number,
-        required: true
+        required: [true, 'Exploitation is required'],
     },
     price: {
         type: Number,
