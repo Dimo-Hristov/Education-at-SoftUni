ff6ff37dda8d1048cbf33d86ea1b86bf381792a5
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-26 13:47:47 +0300

    add decrement button

--- 08 ReactJS/02 Components Basic Idea/props-one/src/components/Counter.js
+++ 08 ReactJS/02 Components Basic Idea/props-one/src/components/Counter.js
@@ -3,14 +3,18 @@ import { useState } from "react";
 const Counter = (props) => {
     const [counter, setCounter] = useState(0);
 
-    const onButtonClick = (e) => {
+    const onIncrementCounter = (e) => {
         setCounter(oldCounter => oldCounter + 1)
+    };
+    const onDecrementtCounter = (e) => {
+        setCounter(oldCounter => oldCounter - 1)
     };
 
     return (
         <div>
             <h3>Counter: {counter}</h3>
-            <button onClick={onButtonClick}>+</button>
+            <button onClick={onIncrementCounter}>+</button>
+            <button onClick={onDecrementtCounter}>-</button>
         </div>
     )
 };
