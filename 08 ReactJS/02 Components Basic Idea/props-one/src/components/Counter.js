457bb0772cf135c3f67241dd3eeced8a04639007
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-26 13:48:49 +0300

    add clear button

--- 08 ReactJS/02 Components Basic Idea/props-one/src/components/Counter.js
+++ 08 ReactJS/02 Components Basic Idea/props-one/src/components/Counter.js
@@ -10,11 +10,16 @@ const Counter = (props) => {
         setCounter(oldCounter => oldCounter - 1)
     };
 
+    const clearCounterHandler = (e) => {
+        setCounter(0)
+    }
+
     return (
         <div>
             <h3>Counter: {counter}</h3>
             <button onClick={onIncrementCounter}>+</button>
             <button onClick={onDecrementtCounter}>-</button>
+            <button onClick={clearCounterHandler}>Clear</button>
         </div>
     )
 };
