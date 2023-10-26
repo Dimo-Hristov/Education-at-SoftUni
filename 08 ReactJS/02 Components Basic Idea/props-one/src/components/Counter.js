33561b6ba806f421023ad13cd77b35bb2efb4e46
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-26 23:09:41 +0300

    complete the demo

--- 08 ReactJS/02 Components Basic Idea/props-one/src/components/Counter.js
+++ 08 ReactJS/02 Components Basic Idea/props-one/src/components/Counter.js
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
-// const getTitle = (count) => {
-//     switch (count) {
-//         case 1: return 'First Blood';
-//         case 2: return 'Double kill';
-//         case 3: return 'Trimple kill';
-//         case 4: return 'Quadra kill';
-//         case 5: return 'Penta kill';
+const getTitle = (count) => {
+    switch (count) {
+        case 1: return 'First Blood';
+        case 2: return 'Double kill';
+        case 3: return 'Tripple kill';
+        case 4: return 'Multikill';
+        case 5: return 'Godlike';
 
-//         default: return 'Counter';
-//     }
-// };
+        default: return 'Counter';
+    }
+};
 
 const Counter = (props) => {
     const [counter, setCounter] = useState(0);
@@ -26,16 +26,16 @@ const Counter = (props) => {
         setCounter(0)
     }
 
-    let title = 'Counter';
-    if (counter === 1) {
-        title = 'First Blood'
-    } else if (counter === 2) {
-        title = 'Double Kill'
-    }
-
+    // let title = 'Counter';
+    // if (counter === 1) {
+    //     title = 'First Blood'
+    // } else if (counter === 2) {
+    //     title = 'Double Kill'
+    // }
+    debugger
     return (
         <div>
-            <h3>{title}: {counter}</h3>
+            <h3>{getTitle(counter) > 5 ? 'Unstoppable' : getTitle(counter)}: {counter}</h3>
             <button onClick={onIncrementCounter}>+</button>
             <button onClick={onDecrementtCounter}>-</button>
             <button onClick={clearCounterHandler}>Clear</button>
