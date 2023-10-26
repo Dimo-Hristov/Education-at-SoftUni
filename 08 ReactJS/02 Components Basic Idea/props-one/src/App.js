77345771630988004e2ad21cfbf2e01f6a39b929
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-26 10:02:17 +0300

    create component and pass data parent to child

--- 08 ReactJS/02 Components Basic Idea/props-one/src/App.js
+++ 08 ReactJS/02 Components Basic Idea/props-one/src/App.js
@@ -1,33 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
-function SimpleParagraph(props) {
-  return <p>Hello: {props.name}</p>
-}
+import Movie from './components/Movie';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <div className="test">
-          <SimpleParagraph name="React" />
-        </div>
-
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Movie title="Man of Steel" />
     </div>
   );
 }
---
+++ 08 ReactJS/02 Components Basic Idea/props-one/src/components/Movie.js
@@ -0,0 +1,5 @@
+function Movie(props) {
+    return <article>{props.title}</article>
+};
+
+export default Movie;
