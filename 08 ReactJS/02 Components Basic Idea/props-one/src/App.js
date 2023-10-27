c8db8034c65f1c24e97b4a2bcd5e6a1579e759cc
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-27 14:55:29 +0300

    export movies object in constants

--- 08 ReactJS/02 Components Basic Idea/props-one/src/App.js
+++ 08 ReactJS/02 Components Basic Idea/props-one/src/App.js
@@ -2,26 +2,11 @@ import './App.css';
 import Counter from './components/Counter';
 import MovieList from './components/MovieList';
 // import Timer from './components/Times';
+import { movies } from './costants';
 
 
 function App() {
-  const movies = [
-    {
-      title: 'Man of steel',
-      year: 2008,
-      cast: ['Henvry Vavil', 'Russell Crowe'],
-    },
-    {
-      title: 'Harry Potter',
-      year: 2011,
-      cast: ['Pesho', 'Gosho'],
-    },
-    {
-      title: 'Rambo',
-      year: 2001,
-      cast: ['Vankata', 'Bat Misho'],
-    }
-  ];
+
 
   return (
     <div className="App">
---
+++ 08 ReactJS/02 Components Basic Idea/props-one/src/costants.js
@@ -0,0 +1,17 @@
+export const movies = [
+    {
+        title: 'Man of steel',
+        year: 2008,
+        cast: ['Henvry Vavil', 'Russell Crowe'],
+    },
+    {
+        title: 'Harry Potter',
+        year: 2011,
+        cast: ['Pesho', 'Gosho'],
+    },
+    {
+        title: 'Rambo',
+        year: 2001,
+        cast: ['Vankata', 'Bat Misho'],
+    }
+];
