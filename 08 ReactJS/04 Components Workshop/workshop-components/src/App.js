485e76771c766696828c1e576d24127875b000d7
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-30 14:36:34 +0200

    create Search component

--- 08 ReactJS/04 Components Workshop/workshop-components/src/App.js
+++ 08 ReactJS/04 Components Workshop/workshop-components/src/App.js
@@ -1,6 +1,7 @@
+import './App.css'
 import Header from "./components/Header";
 import Footer from "./components/footer";
-import './App.css'
+import Search from "./components/Search";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 
       <main className="main">
         <section className="card users-container>">
+          <Search />
 
         </section>
 
