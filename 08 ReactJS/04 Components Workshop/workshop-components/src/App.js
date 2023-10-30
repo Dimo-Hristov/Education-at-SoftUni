7e0528058e9bd5735ff74fa282ca769d1eb07856
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-30 14:20:44 +0200

    create footer components and add module css

--- 08 ReactJS/04 Components Workshop/workshop-components/src/App.js
+++ 08 ReactJS/04 Components Workshop/workshop-components/src/App.js
@@ -1,11 +1,19 @@
 import Header from "./components/Header";
+import Footer from "./components/footer";
+
+
 
 function App() {
   return (
     <>
       <Header />
 
+      <main>
 
+
+      </main>
+
+      <Footer />
     </>
   );
 }
---
+++ 08 ReactJS/04 Components Workshop/workshop-components/src/components/footer.js
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css'
+
+export default function Footer() {
+    return (
+        <footer className={styles['footer']}>
+            <p>Copyright © designed by Mihail Valkov</p>
+        </footer>
+    );
+}
