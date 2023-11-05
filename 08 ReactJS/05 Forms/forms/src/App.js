1da2afb4c0425e20c6aa1b5b8e51d276c7deea05
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-05 11:54:39 +0200

    add controlled username input

--- 08 ReactJS/05 Forms/forms/src/App.js
+++ 08 ReactJS/05 Forms/forms/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [username, setUsername] = useState('')
+
+  const usernameChangeHandler = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const submitHandler = (e) => {
+    e.predentDefault()
+  }
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={submitHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" htmlFor="username" name="username" />
+        <input type="text" htmlFor="username" name="username" value={username} onChange={usernameChangeHandler} />
         <label htmlFor="password">Password</label>
         <input type="text" htmlFor="password" name="password" />
 
