10d95e528e1d716fa425065999970f434919f66b
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-05 12:55:02 +0200

    add register controlled form

--- 08 ReactJS/05 Forms/forms/src/login.js
+++ 08 ReactJS/05 Forms/forms/src/login.js
@@ -22,9 +22,9 @@ function Login() {
         <div className="login">
             <form onSubmit={submitHandler}>
                 <label htmlFor="username">Username</label>
-                <input type="text" htmlFor="username" name="username" value={username} onChange={usernameChangeHandler} />
+                <input type="text" id="username" name="username" value={username} onChange={usernameChangeHandler} />
                 <label htmlFor="password">Password</label>
-                <input type="text" htmlFor="password" name="password" value={password} onChange={passwordChangeHandler} />
+                <input type="text" id="password" name="password" value={password} onChange={passwordChangeHandler} />
 
                 <input type="submit" value="Submit" />
             </form>
--- 08 ReactJS/05 Forms/forms/src/register.js
+++ 08 ReactJS/05 Forms/forms/src/register.js
@@ -1,20 +1,42 @@
-
+import { useState } from "react";
 
 function Register() {
 
+    const [age, setAge] = useState(null);
+
+    const onAgeChangeHandler = (e) => {
+        setAge(e.target.value)
+    }
+
+    const [values, setValues] = useState({
+        email: 'Pesho',
+        password: '123',
+        city: 'Sofia'
+    });
+
+    const onChangeHandler = (e) => {
+        setValues(state => ({ ...state, [e.target.name]: e.target.value }))
+
+    }
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+    }
+
+
     return (
-        <form>
+        <form onSubmit={onSubmitHandler}>
             <label htmlFor="email"></label>
-            <input type="text" htmlFor="email" name="email" />
+            <input type="text" id="email" name="email" onChange={onChangeHandler} value={values.email} />
 
             <label htmlFor="password"></label>
-            <input type="password" htmlFor="password" name="password" />
+            <input type="password" id="password" name="password" onChange={onChangeHandler} value={values.password} />
 
             <label htmlFor="age"></label>
-            <input type="number" htmlFor="age" name="age" />
+            <input type="number" id="age" name="age" onChange={onAgeChangeHandler} value={age || ''} />
 
             <label htmlFor="city"></label>
-            <input type="text" htmlFor="city" name="city" />
+            <input type="text" id="city" name="city" onChange={onChangeHandler} value={values.city} />
 
             <input type="submit" value="Submit" />
 
