3b3fc8f33b3d87c7ad7ad6c153fafcefdbe60a2e
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-07 10:13:25 +0200

    add back button in details page

--- 08 ReactJS/06 Routing/routing-client/src/CharacterDetail.js
+++ 08 ReactJS/06 Routing/routing-client/src/CharacterDetail.js
@@ -1,16 +1,22 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react'
 
 export const CharacterDetails = () => {
     const baseUrl = 'https://swapi.dev/api/people';
     const { characterId } = useParams();
     const [character, setCharacter] = useState({});
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch(`${baseUrl}/${characterId}/`)
             .then(res => res.json())
             .then(data => setCharacter(data))
-    }, [characterId])
+    }, [characterId]);
+
+    const onBackButtonClick = () => {
+        // use -1 to go in the previous page
+        navigate('/characters')
+    }
 
     return (
         <>
@@ -18,6 +24,8 @@ export const CharacterDetails = () => {
 
             <h2>Name: {character.name}</h2>
             <p>Eye color: {character.eye_color}</p>
+            <hr />
+            <button onClick={onBackButtonClick}>Back</button>
         </>
     );
 };
