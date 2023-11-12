d8e0c2ce59fb1dbd4a06574c0019bcf28cf2bc77
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-12 10:06:33 +0200

    add router, create routes

--- 08 ReactJS/07 Workshop Routing/app/src/App.js
+++ 08 ReactJS/07 Workshop Routing/app/src/App.js
@@ -1,12 +1,27 @@
+import { CatalogPage } from "./CatalogPage/CatalogPage";
+import { CreatePage } from "./CreatePage/CreatePage";
+import { DetailsPage } from "./DetailsPage/DetailsPage";
+import { EditPage } from "./EditPage/EditPage";
+import { Register } from "./Register/Register";
+import { Login } from "./Login/Login";
 import { Header } from "./Header/Header";
 import { HomePage } from "./HomePage/HomePage";
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div id="box">
       <Header />
       <main id="main-content">
-        <HomePage />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/catalog' element={<CatalogPage />} />
+          <Route path='/create' element={<CreatePage />} />
+          <Route path='/details' element={<DetailsPage />} />
+          <Route path='/edit' element={<EditPage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
       </main>
 
     </div>
