b040b992047b89b2a8e60d10b91b9a0063a48451
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-12 15:06:56 +0200

    create CategoryItem , pass and render the data.

--- 08 ReactJS/07 Workshop Routing/app/src/components/CatalogPage/CatalogPage.js
+++ 08 ReactJS/07 Workshop Routing/app/src/components/CatalogPage/CatalogPage.js
@@ -1,18 +1,13 @@
-export const CatalogPage = () => {
+import { CartalogItem } from "./CatalogItem/CatalogItem"
+
+export const CatalogPage = ({
+    gamesList
+}) => {
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
             {/* Display div: with information about every game (if any) */}
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">
-                        Details
-                    </a>
-                </div>
-            </div>
+            {gamesList.map((x) => <CartalogItem key={x._id} {...x} />)}
             {/* Display paragraph: If there is no games  */}
             <h3 className="no-articles">No articles yet</h3>
         </section>
