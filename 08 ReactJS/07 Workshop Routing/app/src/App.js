87adeecf5d3bb0229cf441aadb8c12075c801823
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-12 09:48:31 +0200

    Initialize app, add components

---
+++ 08 ReactJS/07 Workshop Routing/app/src/App.js
@@ -0,0 +1,16 @@
+import { Header } from "./Header/Header";
+import { HomePage } from "./HomePage/HomePage";
+
+function App() {
+  return (
+    <div id="box">
+      <Header />
+      <main id="main-content">
+        <HomePage />
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
